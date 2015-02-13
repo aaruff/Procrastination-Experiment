6e5a89c92a6323ccdac8050062ded1a57a791c63
Anwar A. Ruff <anwarruff@gmail.com>
2015-02-13 18:47:41 -0500

    Added Twig extension and config file loading

--- public/index.php
+++ public/index.php
@@ -1,6 +1,20 @@
 <?php
-require __DIR__.'/../vendor/autoload.php';
+define('BASE_DIR', __DIR__ . "/..");
 
-$app = new \Slim\Slim(array(
-    'view' => new \Slim\Views\Twig()
-));
+require BASE_DIR . '/vendor/autoload.php';
+
+$config = require BASE_DIR . '/config/app.php';
+$app = new Slim\Slim($config);
+
+require BASE_DIR . '/app/routes.php';
+
+$view = $app->view();
+$view->parserOptions = array(
+    'debug' => true,
+    'cache' => BASE_DIR . '/storage/cache'
+);
+$view->parserExtensions = array(
+    new \Slim\Views\TwigExtension(),
+);
+
+$app->run();
