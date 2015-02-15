fcf32d2585a28b1e4dba57581ccca682e41d5d79
Anwar A. Ruff <anwarruff@gmail.com>
2015-02-14 23:35:56 -0500

    Created Base Controller

---
+++ app/Officium/Controllers/BaseController.php
@@ -0,0 +1,29 @@
+<?php
+namespace Officium\Controllers;
+
+
+class BaseController {
+    protected $app;
+
+    public function __construct() {
+        global $app;
+        $this->app = $app;
+    }
+
+    /**
+     * Simplify the call to the view render and enable dot syntax for file paths
+     * @param $template
+     * @param $params
+     */
+    protected function view($template, $params = []){
+        /**
+         * Enable use to pass in the path to the view as '/' or '.' for example
+         * 'foldername/viewname'
+         * 'foldername.viewname' this will parse as 'foldername/viewname'
+         * then append a .php to the file name
+         */
+        $view = str_replace('.', '/', $template);
+        $this->app->render($view . '.twig', $params);
+    }
+
+}
