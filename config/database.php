17932a65249497249dcdf94e5a5309422ff9b786
Anwar A. Ruff <anwarruff@gmail.com>
2015-02-14 23:38:21 -0500

    Created database config file

---
+++ config/database.php
@@ -0,0 +1,12 @@
+<?php
+return array(
+    'driver' => 'mysql',
+    'host' => getenv('DB_HOST'),
+    'port' => '33060',
+    'database' => getenv('DB_NAME'),
+    'username' => getenv('DB_USERNAME'),
+    'password' => getenv('DB_PASSWORD'),
+    'charset' => 'utf8',
+    'collation' => 'utf8_unicode_ci',
+    'prefix' => '',
+);
