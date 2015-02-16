8a7bffbc10c0f04535cce72d0982d2a8e89194e3
Anwar A. Ruff <anwarruff@gmail.com>
2015-02-15 23:55:10 -0500

    Added experimenter login post to route

--- app/routes.php
+++ app/routes.php
@@ -3,3 +3,4 @@ $app->get('/researcher/login', function() {
     echo "Login";
 });
 $app->get('/experimenter/login', 'Officium\Controllers\Experimenter\Login:get');
+$app->post('/experimenter/login', 'Officium\Controllers\Experimenter\Login:post');
