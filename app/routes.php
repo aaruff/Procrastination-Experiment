f74c90eb2989a0969169b870e21f3abc82ce17b2
Anwar A. Ruff <anwarruff@gmail.com>
2015-02-16 23:48:11 -0500

    Removed unused routes

--- app/routes.php
+++ app/routes.php
@@ -1,6 +1,3 @@
 <?php
-$app->get('/researcher/login', function() {
-    echo "Login";
-});
 $app->get('/experimenter/login', 'Officium\Controllers\Experimenter\Login:get');
 $app->post('/experimenter/login', 'Officium\Controllers\Experimenter\Login:post');
