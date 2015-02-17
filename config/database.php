f6427ac28161e5eca036558bb6c171457f90e6dc
Anwar A. Ruff <anwarruff@gmail.com>
2015-02-16 23:49:15 -0500

    Updated database config with env vars

--- config/database.php
+++ config/database.php
@@ -3,7 +3,7 @@ return array(
     'driver' => 'mysql',
     'host' => getenv('DB_HOST'),
     'port' => '33060',
-    'database' => getenv('DB_NAME'),
+    'database' => getenv('DB_DATABASE'),
     'username' => getenv('DB_USERNAME'),
     'password' => getenv('DB_PASSWORD'),
     'charset' => 'utf8',
