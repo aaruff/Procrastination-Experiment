617fc9103c39b49bcf60d2fad38338170472248d
Anwar A. Ruff <anwarruff@gmail.com>
2015-02-17 15:10:21 -0500

    Added dashboard and session get routes

--- app/routes.php
+++ app/routes.php
@@ -1,3 +1,5 @@
 <?php
 $app->get('/experimenter/login', 'Officium\Controllers\Experimenter\Login:get');
 $app->post('/experimenter/login', 'Officium\Controllers\Experimenter\Login:post');
+$app->get('/experiment/dashboard', 'Officium\Controllers\Experimenter\Experiment\Dashboard:get');
+$app->get('/experiment/session', 'Officium\Controllers\Experimenter\Experiment\Session:get');
