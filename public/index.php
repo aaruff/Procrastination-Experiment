94738a754e97bd62c3e5229d462c9299519a16be
Anwar A. Ruff <anwarruff@gmail.com>
2015-02-17 15:11:31 -0500

    Added template and cookie handling

--- public/index.php
+++ public/index.php
@@ -3,27 +3,30 @@ define('BASE_DIR', __DIR__ . "/..");
 
 require BASE_DIR . '/vendor/autoload.php';
 
+// Environment
 Dotenv::load(BASE_DIR);
 
+// Config
 $appConfig = require BASE_DIR . '/config/app.php';
 $app = new Slim\Slim($appConfig);
 
+// Database
 $dbConfig = require BASE_DIR . '/config/database.php';
 $dbManager = new Illuminate\Database\Capsule\Manager();
 $dbManager->addConnection($dbConfig);
 $dbManager->setAsGlobal();
 $dbManager->bootEloquent();
 
-
+// Routes
 require BASE_DIR . '/app/routes.php';
 
-$view = $app->view();
-$view->parserOptions = array(
-    'debug' => true,
-    'cache' => BASE_DIR . '/storage/cache'
-);
-$view->parserExtensions = array(
-    new \Slim\Views\TwigExtension(),
-);
+// Template
+$template = $app->view();
+$template->parserOptions = require BASE_DIR . '/config/twig.php';
+$template->parserExtensions = array(new \Slim\Views\TwigExtension());
+
+// Cookies (4kb limit)
+$cookiesConfig = require BASE_DIR . '/config/cookies.php';
+$app->add(new \Slim\Middleware\SessionCookie($cookiesConfig));
 
 $app->run();
