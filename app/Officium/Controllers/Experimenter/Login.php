81eed73734d53c5e11459e840cb72ac1e909e502
Anwar A. Ruff <anwarruff@gmail.com>
2015-02-16 23:48:00 -0500

    Added login validation

--- app/Officium/Controllers/Experimenter/Login.php
+++ app/Officium/Controllers/Experimenter/Login.php
@@ -2,16 +2,42 @@
 
 namespace Officium\Controllers\Experimenter;
 
+use Valitron\Validator as Validator;
 use Officium\Controllers\BaseController;
+use Illuminate\Database\Capsule\Manager as Database;
 
-class Login extends BaseController {
+class Login extends BaseController
+{
 
-    function __construct() {
+    function __construct()
+    {
         parent::__construct();
     }
 
     public function get()
     {
-        $this->view('pages.experimenter.login');
+        $this->render('pages.experimenter.login');
+    }
+
+    public function post()
+    {
+        $postEntries = $this->getPost();
+        $validator = $this->getValidator($postEntries);
+        if ( ! $validator->validate()) {
+            $this->render('pages.experimenter.login', ['errors'=>$validator->errors()]);
+        }
+
+        $user = Database::table('experimenter')->where('login', '=', $postEntries['login'])->first();
+    }
+
+    private function getValidator($postEntries)
+    {
+        $validator = new Validator($postEntries);
+        $validator->rule('required', ['login', 'password']);
+        $validator->rule('alpha', 'login');
+        $validator->rule('lengthMin', '3');
+        $validator->rule('lengthMin', '30');
+
+        return $validator;
     }
 }
