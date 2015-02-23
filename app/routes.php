08fa9c510f95320dc213b8e771fe1555aad47404
Anwar A. Ruff <anwarruff@gmail.com>
2015-02-23 16:34:44 -0500

    Added session post to routes

--- app/routes.php
+++ app/routes.php
@@ -3,3 +3,4 @@ $app->get('/experimenter/login', 'Officium\Controllers\Experimenter\Login:get');
 $app->post('/experimenter/login', 'Officium\Controllers\Experimenter\Login:post');
 $app->get('/experiment/dashboard', 'Officium\Controllers\Experimenter\Experiment\Dashboard:get');
 $app->get('/experiment/session', 'Officium\Controllers\Experimenter\Experiment\Session:get');
+$app->post('/experiment/session', 'Officium\Controllers\Experimenter\Experiment\Session:post');
