4caebdc7b955652b8ea240c7770284a7298dc1c2
Anwar A. Ruff <anwarruff@gmail.com>
2015-02-23 16:38:25 -0500

    Added cookie config

--- config/app.php
+++ config/app.php
@@ -1,7 +1,8 @@
 <?php
 
 return array(
-    'view' => new \Slim\Views\Twig(),
+    'view' => (new \Slim\Views\Twig()),
+    'base_url' => 'http://officium:8000/',
     'debug' => true,
     'templates.path' => BASE_DIR .'/resources/views',
     'pdo' => array(
@@ -12,4 +13,9 @@ return array(
             'options' => array()
         ),
     ),
+    // cookie config
+    'cookies.secret_key' => getenv('SECRET_KEY'),
+    'cookies.lifetime' => getenv('SECRET_KEY'),
+    'cookies.cipher' => MCRYPT_RIJNDAEL_256,
+    'cookies.cipher_mode' => MCRYPT_MODE_CBC
 );
