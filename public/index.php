bbf5873af409f7b1dae8ceb709900d76abdccc5e
Anwar A. Ruff <anwarruff@gmail.com>
2015-02-23 16:39:45 -0500

    Added eloquent & comments

--- public/index.php
+++ public/index.php
@@ -3,29 +3,47 @@ define('BASE_DIR', __DIR__ . "/..");
 
 require BASE_DIR . '/vendor/autoload.php';
 
-// Environment
+/*---------------------------------------------------
+ * Environment
+ *--------------------------------------------------- */
 Dotenv::load(BASE_DIR);
 
-// Config
+/*---------------------------------------------------
+ * Slim
+ *--------------------------------------------------- */
 $appConfig = require BASE_DIR . '/config/app.php';
 $app = new Slim\Slim($appConfig);
 
-// Database
+/*---------------------------------------------------
+ * Database
+ *--------------------------------------------------- */
 $dbConfig = require BASE_DIR . '/config/database.php';
-$dbManager = new Illuminate\Database\Capsule\Manager();
-$dbManager->addConnection($dbConfig);
-$dbManager->setAsGlobal();
-$dbManager->bootEloquent();
+use Illuminate\Database\Capsule\Manager as Capsule;
+$capsule = new Capsule;
+$capsule->addConnection($dbConfig);
 
-// Routes
+use Illuminate\Events\Dispatcher;
+use Illuminate\Container\Container;
+$capsule->setEventDispatcher(new Dispatcher(new Container));
+$capsule->setAsGlobal();
+
+$capsule->bootEloquent();
+
+/*---------------------------------------------------
+ * Routes
+ *--------------------------------------------------- */
 require BASE_DIR . '/app/routes.php';
 
-// Template
+/*---------------------------------------------------
+ * Twig
+ *--------------------------------------------------- */
 $template = $app->view();
 $template->parserOptions = require BASE_DIR . '/config/twig.php';
 $template->parserExtensions = array(new \Slim\Views\TwigExtension());
 
-// Cookies (4kb limit)
+/*---------------------------------------------------
+ * Cookies
+ *--------------------------------------------------- */
 $cookiesConfig = require BASE_DIR . '/config/cookies.php';
 $app->add(new \Slim\Middleware\SessionCookie($cookiesConfig));
 
