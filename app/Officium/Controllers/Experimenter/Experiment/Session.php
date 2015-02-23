38d9b51d625f2d3980f6c06e6e1e87c5c9d1a7b5
Anwar A. Ruff <anwarruff@gmail.com>
2015-02-23 16:36:58 -0500

    Added post request handler

--- app/Officium/Controllers/Experimenter/Experiment/Session.php
+++ app/Officium/Controllers/Experimenter/Experiment/Session.php
@@ -2,7 +2,7 @@
 
 namespace Officium\Controllers\Experimenter\Experiment;
 
-
+use Officium\Models\Experiment;
 use Officium\Controllers\Experimenter\BaseController;
 
 class Session extends BaseController
@@ -17,4 +17,18 @@ class Session extends BaseController
         $this->render('pages.experimenter.experiment.session');
     }
 
+    public function post()
+    {
+        $postEntries = $this->getPost();
+
+        // Error Handling
+        $errors = Experiment::validate($postEntries);
+        if ( ! empty($errors)) {
+            var_dump($errors);
+            die();
+            $this->app->flash('errors', $errors);
+            $this->app->redirect(Login::route());
+            return;
+        }
+    }
 }
