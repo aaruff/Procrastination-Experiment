665dbb06baeb306a662776a44a28a6fdf4cff721
Anwar A. Ruff <anwarruff@gmail.com>
2015-02-27 15:52:58 -0500

    Moved the base controller to the controller root directory

---
+++ app/Officium/Controllers/BaseController.php
@@ -0,0 +1,52 @@
+<?php
+namespace Officium\Controllers;
+
+class BaseController
+{
+    protected function redirectToLogin()
+    {
+        $this->app->redirect(Login::route());
+    }
+
+
+    protected function loginResearcher($experimenter)
+    {
+        $_SESSION['experimenter'] = $experimenter;
+    }
+
+    protected function isResearcher()
+    {
+        return isset($_SESSION['experimenter']);
+    }
+
+    protected function logoutResearcher()
+    {
+        unset($_SESSION['experimenter']);
+    }
+
+    protected function loginSubject($subject)
+    {
+        $_SESSION['subject'] = $subject;
+    }
+
+    protected function isSubject()
+    {
+        return isset($_SESSION['subject']);
+    }
+
+    protected function logoutSubject()
+    {
+        unset($_SESSION['subject']);
+    }
+
+    /**
+     * Add dot notation to paths
+     * @param $template
+     * @param $params
+     */
+    protected function render($template, $params = []){
+        $view = str_replace('.', '/', $template);
+        App::render($view . '.twig', $params);
+    }
+
+}
