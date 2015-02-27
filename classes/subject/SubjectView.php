0c52796d413bcb445a914ac630ad54edb6f84b60
Anwar A. Ruff <anwarruff@gmail.com>
2015-02-27 16:08:14 -0500

    Deleted legacy classes that have been replaced via slim

--- classes/subject/SubjectView.php
+++
@@ -1,8 +0,0 @@
-<?php
-class SubjectView extends View {
-	private $subject_view_path = '../views/subject/';
-	
-	public function __construct(){
-		parent::__construct($this->subject_view_path);
-	}
-}
