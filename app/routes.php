2e9f65d0cc49f2c5e87d0545e294b0bbce65e6ce
Anwar A. Ruff <anwarruff@gmail.com>
2015-02-27 15:53:47 -0500

    Added session and subject login routes

--- app/routes.php
+++ app/routes.php
@@ -1,6 +1,17 @@
 <?php
-$app->get('/experimenter/login', 'Officium\Controllers\Experimenter\Login:get');
-$app->post('/experimenter/login', 'Officium\Controllers\Experimenter\Login:post');
-$app->get('/experiment/dashboard', 'Officium\Controllers\Experimenter\Experiment\Dashboard:get');
-$app->get('/experiment/session', 'Officium\Controllers\Experimenter\Experiment\Session:get');
-$app->post('/experiment/session', 'Officium\Controllers\Experimenter\Experiment\Session:post');
+
+Route::get('/subject/login', '\Officium\Controllers\Subject\Login:get');
+Route::post('/subject/login', '\Officium\Controllers\Subject\Login:post');
+
+/**
+ * Experimenter Routes
+ */
+Route::get('/experimenter/login', '\Officium\Controllers\Experimenter\Login:get');
+Route::post('/experimenter/login', 'Officium\Controllers\Experimenter\Login:post');
+
+Route::get('/experiment/dashboard', 'Officium\Controllers\Experimenter\Experiment\Dashboard:get');
+Route::post('/experiment/dashboard', 'Officium\Controllers\Experimenter\Experiment\Dashboard:post');
+
+Route::get('/experiment/session/:id', 'Officium\Controllers\Experimenter\Experiment\Session:show');
+Route::get('/experiment/session/', 'Officium\Controllers\Experimenter\Experiment\Session:get');
+Route::post('/experiment/session/create', 'Officium\Controllers\Experimenter\Experiment\Session:create');
