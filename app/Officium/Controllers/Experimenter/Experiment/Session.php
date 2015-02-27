6ec9f9dbe17a91372b0adc1425d1ccaf4d47d7c5
Anwar A. Ruff <anwarruff@gmail.com>
2015-02-27 15:55:24 -0500

    Added session creation logic

--- app/Officium/Controllers/Experimenter/Experiment/Session.php
+++ app/Officium/Controllers/Experimenter/Experiment/Session.php
@@ -2,33 +2,55 @@
 
 namespace Officium\Controllers\Experimenter\Experiment;
 
-use Officium\Models\Experiment;
-use Officium\Controllers\Experimenter\BaseController;
+use Officium\Models\Subject;
+use Officium\Models\Session as ExperimentSession;
+use Officium\Controllers\BaseController;
 
 class Session extends BaseController
 {
-    public function __construct()
-    {
-        parent::__construct();
-    }
-
     public function get()
     {
-        $this->render('pages.experimenter.experiment.session');
+        $this->render('pages.experimenter.experiment.session.create');
     }
 
-    public function post()
+    public function create()
     {
-        $postEntries = $this->getPost();
+        $post = Request::post();
 
-        // Error Handling
-        $errors = Experiment::validate($postEntries);
+        $errors = ExperimentSession::validate($post);
         if ( ! empty($errors)) {
-            var_dump($errors);
-            die();
-            $this->app->flash('errors', $errors);
-            $this->app->redirect(Login::route());
+            App::flash('errors', $errors);
+            Response::redirect(Session::route());
             return;
         }
+
+        $session = ExperimentSession::create($post);
+        $size = intval($post['size']);
+
+        for ($i = 0; $i < $size; ++$i) {
+            $subject = new Subject();
+            $subject->login = $subject->generateLogin();
+            $subject->password = password_hash($session->id . $subject->login, PASSWORD_DEFAULT);
+            $subject->session_id = $session->id;
+            $subject->status = Subject::$UNREGISTERED;
+            $subject->save();
+        }
+        App::redirect(Dashboard::route());
+    }
+
+    public function show($id='')
+    {
+        $session = ExperimentSession::validateId($id);
+        if ( ! $session) {
+            Response::redirect(Session::route());
+            return;
+        }
+
+        $this->render('pages.experimenter.experiment.session.show', ['session'=>$session, 'subjects'=>$session->subjects]);
+    }
+
+    public static function route()
+    {
+        return '/experiment/session';
     }
 }
