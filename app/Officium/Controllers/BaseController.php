d81a4c0ee48924933edf97ebbd99ea564e0ee302
Anwar A. Ruff <anwarruff@gmail.com>
2015-03-12 16:41:59 -0400

    refactored methods into the Subject Base and Researcher Base controllers.

--- app/Officium/Controllers/BaseController.php
+++ app/Officium/Controllers/BaseController.php
@@ -3,47 +3,6 @@ namespace Officium\Controllers;
 
 class BaseController
 {
-    protected function redirectToLogin()
-    {
-        $this->app->redirect(Login::route());
-    }
-
-
-    protected function loginResearcher($experimenter)
-    {
-        $_SESSION['experimenter'] = $experimenter;
-    }
-
-    protected function isResearcher()
-    {
-        return isset($_SESSION['experimenter']);
-    }
-
-    protected function logoutResearcher()
-    {
-        unset($_SESSION['experimenter']);
-    }
-
-    protected function loginSubject($subject)
-    {
-        $_SESSION['subject'] = $subject;
-    }
-
-    protected function getSubject()
-    {
-        return $_SESSION['subject'];
-    }
-
-    protected function isSubject()
-    {
-        return isset($_SESSION['subject']);
-    }
-
-    protected function logoutSubject()
-    {
-        unset($_SESSION['subject']);
-    }
-
     /**
      * Add dot notation to paths
      * @param $template
