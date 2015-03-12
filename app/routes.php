65e445b43c84e64e092670f6ee9f60c3cbc12bf5
Anwar A. Ruff <anwarruff@gmail.com>
2015-03-12 13:01:03 -0400

    Updated routes to reflect controller renaming

--- app/routes.php
+++ app/routes.php
@@ -2,23 +2,23 @@
 //---------------------------------------------------
 // Subject Routes
 //---------------------------------------------------
-Route::get('/subject/login', '\Officium\Controllers\Subject\Login:get');
-Route::post('/subject/login', '\Officium\Controllers\Subject\Login:post');
+Route::get('/subject/login', '\Officium\Controllers\Subject\LoginController:get');
+Route::post('/subject/login', '\Officium\Controllers\Subject\LoginController:post');
 
 // Incoming Questionnaire
-Route::get('/subject/questionnaire/a', '\Officium\Controllers\Subject\Questionnaire\Academic:get');
-Route::post('/subject/questionnaire/a', '\Officium\Controllers\Subject\Questionnaire:Academic:post');
-Route::get('/subject/questionnaire/ao', '\Officium\Controllers\Subject\Questionnaire\AcademicObligation:get');
-Route::post('/subject/questionnaire/ao', '\Officium\Controllers\Subject\Questionnaire:AcademicObligation:post');
+Route::get('/subject/questionnaire/a', '\Officium\Controllers\Subject\Questionnaire\AcademicController:get');
+Route::post('/subject/questionnaire/a', '\Officium\Controllers\Subject\Questionnaire:AcademicController:post');
+Route::get('/subject/questionnaire/ao', '\Officium\Controllers\Subject\Questionnaire\AcademicObligationController:get');
+Route::post('/subject/questionnaire/ao', '\Officium\Controllers\Subject\Questionnaire:AcademicObligationController:post');
 
 
 //---------------------------------------------------
 // Experimenter Routes
 //---------------------------------------------------
-Route::get('/experimenter/login', '\Officium\Controllers\Experimenter\Login:get');
-Route::post('/experimenter/login', 'Officium\Controllers\Experimenter\Login:post');
-Route::get('/experiment/dashboard', 'Officium\Controllers\Experimenter\Experiment\Dashboard:get');
-Route::post('/experiment/dashboard', 'Officium\Controllers\Experimenter\Experiment\Dashboard:post');
-Route::get('/experiment/session/:id', 'Officium\Controllers\Experimenter\Experiment\Session:show');
-Route::get('/experiment/session/', 'Officium\Controllers\Experimenter\Experiment\Session:get');
-Route::post('/experiment/session/create', 'Officium\Controllers\Experimenter\Experiment\Session:create');
+Route::get('/experimenter/login', '\Officium\Controllers\Experimenter\LoginController:get');
+Route::post('/experimenter/login', 'Officium\Controllers\Experimenter\LoginController:post');
+Route::get('/experiment/dashboard', 'Officium\Controllers\Experimenter\Experiment\DashboardController:get');
+Route::post('/experiment/dashboard', 'Officium\Controllers\Experimenter\Experiment\DashboardController:post');
+Route::get('/experiment/session/:id', 'Officium\Controllers\Experimenter\Experiment\SessionController:show');
+Route::get('/experiment/session/', 'Officium\Controllers\Experimenter\Experiment\SessionController:get');
+Route::post('/experiment/session/create', 'Officium\Controllers\Experimenter\Experiment\SessionController:create');
