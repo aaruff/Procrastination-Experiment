1a7c7a2ce382c6454146fd35edc53d6b3d133f3b
Anwar A. Ruff <anwarruff@gmail.com>
2015-03-13 13:10:03 -0400

    Added redirect and update login

---
+++ app/Officium/Controllers/Subject/SubjectBaseController.php
@@ -0,0 +1,54 @@
+<?php
+namespace Officium\Controllers\Subject;
+
+use Officium\Models\Subject;
+use Officium\Controllers\Subject\SubjectLoginController as Login;
+
+class SubjectBaseController
+{
+    protected $app;
+    protected $request;
+    protected $response;
+
+    public function __construct()
+    {
+        $this->app = \Slim\Slim::getInstance();
+        $this->request = $this->app->request;
+        $this->response = $this->app->response;
+    }
+
+    protected function redirectToLogin()
+    {
+        $this->app->redirect(Login::route());
+    }
+
+    protected function login($credentials)
+    {
+        $subject = Subject::where('login', '=', $credentials['login'])->first();
+        $_SESSION['subject'] = $subject;
+
+        return $subject;
+    }
+
+    protected function redirect(Subject $subject)
+    {
+        if ($subject->status == 0) {
+            $this->app->redirect('/subject/questionnaire/a');
+        }
+    }
+
+    protected function getSubject()
+    {
+        return Subject::where('id', '=', $_SESSION['subject'])->first();
+    }
+
+    protected function isLoggedIn()
+    {
+        return isset($_SESSION['subject']);
+    }
+
+    protected function logout()
+    {
+        if (isset($_SESSION['subject'])) unset($_SESSION['subject']);
+    }
+}
