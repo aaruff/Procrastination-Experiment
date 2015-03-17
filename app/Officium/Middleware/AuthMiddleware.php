cb754086273138062e5a82ad95f49b920a8dcb94
Anwar A. Ruff <anwarruff@gmail.com>
2015-03-16 23:44:29 -0400

    created auth middleware class

---
+++ app/Officium/Middleware/AuthMiddleware.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: aruff
+ * Date: 3/13/15
+ * Time: 11:47 AM
+ */
+
+namespace Officium\Middleware;
+
+use \Slim\Middleware;
+
+
+class AuthMiddleware extends Middleware
+{
+
+    /**
+     * Call
+     *
+     * Perform actions specific to this middleware and optionally
+     * call the next downstream middleware.
+     */
+    public function call()
+    {
+    }
+}
