9a1847c6ed69a5ac39b0891a1be632f9996872da
Anwar A. Ruff <anwarruff@gmail.com>
2015-03-16 23:47:40 -0400

    Updated renamed classes and view path

--- app/Officium/Controllers/Experimenter/Experiment/SessionController.php
+++ app/Officium/Controllers/Experimenter/Experiment/SessionController.php
@@ -4,23 +4,23 @@ namespace Officium\Controllers\Experimenter\Experiment;
 
 use Officium\Models\Subject;
 use Officium\Models\Session as ExperimentSession;
-use Officium\Controllers\ExperimenterBaseController;
+use Officium\Controllers\Experimenter\ExperimenterBaseController;
 
-class SessionControllerExperimenter extends ExperimenterBaseController
+class SessionController extends ExperimenterBaseController
 {
     public function get()
     {
-        $this->render('pages.experimenter.experiment.session.create');
+        $this->app->render('/pages/experimenter/experiment/session/create.twig');
     }
 
     public function create()
     {
-        $post = Request::post();
+        $post = $this->request->post();
 
         $errors = ExperimentSession::validate($post);
         if ( ! empty($errors)) {
-            App::flash('errors', $errors);
-            Response::redirect(SessionControllerExperimenter::route());
+            $this->app->flash('errors', $errors);
+            $this->response->redirect(SessionController::route());
             return;
         }
 
@@ -35,18 +35,18 @@ class SessionControllerExperimenter extends ExperimenterBaseController
             $subject->status = Subject::$UNREGISTERED;
             $subject->save();
         }
-        App::redirect(DashboardControllerExperimenter::route());
+        $this->app->redirect(DashboardController::route());
     }
 
     public function show($id='')
     {
         $session = ExperimentSession::validateId($id);
         if ( ! $session) {
-            Response::redirect(SessionControllerExperimenter::route());
+            $this->response->redirect(SessionController::route());
             return;
         }
 
-        $this->render('pages.experimenter.experiment.session.show', ['session'=>$session, 'subjects'=>$session->subjects]);
+        $this->app->render('/pages/experimenter/experiment/session/show.twig', ['session'=>$session, 'subjects'=>$session->subjects]);
     }
 
     public static function route()
