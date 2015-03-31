4afb11d2ad3125f2319d3c8eb7e3eb08fdbac805
Anwar A. Ruff <anwarruff@gmail.com>
2015-03-31 13:23:24 -0400

    Created a survey router

---
+++ app/Officium/Routers/SurveyRouter.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Officium\Routers;
+
+
+class SurveyRouter
+{
+    private static $templates = [
+        'a' => 'academic.twig', 'ao' => 'academicObligations.twig', 'eo' => 'externalObligations.twig'];
+    private static $surveyOrder = ['a', 'ao', 'eo'];
+
+    public static function getTemplateRoute($id)
+    {
+        return '/pages/subject/survey/' . self::$templates[$id];
+    }
+
+    public static function getNextSurvey($id)
+    {
+        return array_search($id, self::$surveyOrder);
+    }
+
+}
