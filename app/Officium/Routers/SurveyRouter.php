35d823fb94dc3cfb6b0d943f81c8c598f036e2bd
Anwar A. Ruff <anwarruff@gmail.com>
2015-04-01 18:37:07 -0400

    Finished implementing getNextSurveyId

--- app/Officium/Routers/SurveyRouter.php
+++ app/Officium/Routers/SurveyRouter.php
@@ -2,28 +2,48 @@
 
 namespace Officium\Routers;
 
-
+/**
+ * Survey Route Metadata Manager
+ * Class SurveyRouter
+ * @package Officium\Routers
+ */
 class SurveyRouter
 {
     private static $templates = ['a' => 'academic.twig', 'ao' => 'academicObligations.twig', 'eo' => 'externalObligations.twig'];
     private static $surveyOrder = ['a', 'ao', 'eo'];
 
+    /**
+     * Returns the corresponding template route for the provided id if set, otherwise
+     * the first survey template route is returned.
+     *
+     * @param $id
+     * @return string template route
+     */
     public static function getTemplateRoute($id)
     {
-        return '/pages/subject/survey/' . self::$templates[$id];
-    }
-
-    public static function getNextSurvey($id)
-    {
-        return array_search($id, self::$surveyOrder);
-    }
-
-    public static function getUri($id = '')
-    {
-        if (empty($id)) {
-            return '/survey/:id';
+        $rootPath = '/pages/subject/survey/';
+        if ( isset($id) && isset(self::$templates[$id])) {
+            return $rootPath . self::$templates[$id];
         }
 
-        return '/survey/' . $id;
+        return $rootPath . self::$templates['a'];
+    }
+
+    /**
+     * Returns the next survey ID if it exists, otherwise null is returned.
+     *
+     * @param $id
+     * @return mixed survey ID or null
+     */
+    public static function getNextSurveyId($id)
+    {
+        foreach (self::$surveyOrder as $i => $k) {
+            // Found the survey ID, and there are more surveys to be completed
+            if ($k == $id && $i + 1 < count(self::$surveyOrder)) {
+                return self::$surveyOrder[$i + 1];
+            }
+        }
+
+        return null;
     }
 }
