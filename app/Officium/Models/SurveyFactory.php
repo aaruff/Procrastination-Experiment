9f4a67d1c814aa279e913d35935d65be0c476a1a
Anwar A. Ruff <anwarruff@gmail.com>
2015-04-02 15:55:06 -0400

    Implemented make method, and adde doc blocks

--- app/Officium/Models/SurveyFactory.php
+++ app/Officium/Models/SurveyFactory.php
@@ -3,13 +3,37 @@
 namespace Officium\Models;
 
 
+use Officium\Routers\SurveyRouter;
+
+/**
+ * Class SurveyFactoryTest
+ *
+ * @package Officium\Models
+ */
 class SurveyFactory
 {
+    /**
+     * @var array
+     */
     private static $surveys = [
         'a' => 'GeneralAcademicSurvey', 'ao' => 'AcademicObligationSurvey', 'eo' => 'ExternalObligationSurvey'];
 
-    public static function make($id)
+    /**
+     * Produces the Survey for the corresponding ID. If an invalid id is provided the first survey is returned.
+     *
+     * @param $id
+     * @param $data
+     * @return Survey
+     */
+    public static function make($id, $data)
     {
-        return 'Officium\\Models\\' . self::$surveys[$id];
+        if (SurveyRouter::isSurveyId($id)) {
+            $surveyClass = 'Officium\\Models\\' . self::$surveys[$id];
+            return new $surveyClass($data);
+        }
+
+        $firstSurveyId = SurveyRouter::getNextSurveyId();
+        $surveyClass = 'Officium\\Models\\' . self::$surveys[$firstSurveyId];
+        return new $surveyClass($data);
     }
 }
