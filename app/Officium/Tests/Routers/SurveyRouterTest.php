8ec0cb81c8b82d6ba3823dd2c4556755293437f3
Anwar A. Ruff <anwarruff@gmail.com>
2015-04-02 15:53:36 -0400

    Added doc blocks

---
+++ app/Officium/Tests/Routers/SurveyRouterTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Officium\Tests\Routers;
+
+use Officium\Routers\SurveyRouter;
+
+/**
+ * Class SurveyRouterTest
+ * @package Tests\Officium\Routers
+ */
+class SurveyRouterTest extends \PHPUnit_Framework_TestCase {
+
+    /**
+     * @test
+     */
+    public function When_SurveyKeyDoesNotExists_FirstSurveyTemplateRouteReturned()
+    {
+        $this->assertEquals('/pages/subject/survey/academic.twig', SurveyRouter::getTemplateRoute(null));
+    }
+
+    /**
+     * @test
+     */
+    public function When_InvalidSurveyProvided_FirstSurveyTemplateRouteReturned()
+    {
+        $this->assertEquals('/pages/subject/survey/academic.twig', SurveyRouter::getTemplateRoute('wrong!'));
+    }
+
+    /**
+     * @test
+     */
+    public function When_SurveyValidKeyProvided_CorrespondingSurveyTemplateRouteReturned()
+    {
+        $this->assertEquals('/pages/subject/survey/academic.twig', SurveyRouter::getTemplateRoute('a'));
+        $this->assertEquals('/pages/subject/survey/academicObligations.twig', SurveyRouter::getTemplateRoute('ao'));
+        $this->assertEquals('/pages/subject/survey/externalObligations.twig', SurveyRouter::getTemplateRoute('eo'));
+    }
+
+    /**
+     * @test
+     */
+    public function When_SurveyIdProvided_NextIdReturned()
+    {
+        $this->assertEquals('a', SurveyRouter::getNextSurveyId());
+        $this->assertEquals('a', SurveyRouter::getNextSurveyId(null));
+        $this->assertEquals('ao', SurveyRouter::getNextSurveyId('a'));
+        $this->assertEquals('eo', SurveyRouter::getNextSurveyId('ao'));
+    }
+}
