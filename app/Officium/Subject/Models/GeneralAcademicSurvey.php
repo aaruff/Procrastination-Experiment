8d0962bfb208cbbdf1f3a37a31e842df5f589361
Anwar A. Ruff <anwarruff@gmail.com>
2015-04-06 18:05:30 -0400

    Updated validate func

--- app/Officium/Subject/Models/GeneralAcademicSurvey.php
+++ app/Officium/Subject/Models/GeneralAcademicSurvey.php
@@ -7,41 +7,48 @@ use Respect\Validation\Validator as Validator;
 
 class GeneralAcademicSurvey extends Survey
 {
+    private $MAJOR = 'major';
+    private $GPA = 'gpa';
+    private $NUM_COURSES = 'number_courses';
+    private $NUM_CLUBS = 'number_clubs';
 
+    /**
+     * Sets up survey
+     *
+     * @param array $entries
+     */
     public function __construct($entries)
     {
-        parent::__construct(['major', 'gpa', 'number_courses', 'number_clubs']);
-
-        $this->setEntries($entries);
+        $keys = [$this->MAJOR, $this->GPA, $this->NUM_COURSES, $this->NUM_CLUBS];
+        parent::__construct($keys, $entries);
     }
 
     /**
      * Validates general academic form entries.
-     * @return array
+     * @return boolean
      */
     public function validate()
     {
         // Validate input
-        $valid = false;
         try {
             Validator::arr()
-                ->key('major', Validator::notEmpty()->alpha()->length(1, 255))
-                ->key('gpa', Validator::notEmpty()->int()->between(0, 4))
-                ->key('number_courses', Validator::notEmpty()->int()->between(0, 20))
-                ->key('number_clubs', Validator::notEmpty()->int()->between(0, 20))
+                ->key($this->MAJOR, Validator::notEmpty()->alpha()->length(1, 255, true))
+                ->key($this->GPA, Validator::float()->between(0.0, 4.0, true))
+                ->key($this->NUM_COURSES, Validator::int()->between(0, 20, true))
+                ->key($this->NUM_CLUBS, Validator::int()->between(0, 20, true))
                 ->assert($this->getEntries());
-            $valid = true;
+            return true;
         } // Handle authentication errors
         catch (ValidationException $e) {
             $this->setErrors($e->findMessages([
-                'major' => 'Invalid major',
-                'gpa' => 'Invalid GPA',
-                'number_coursers' => 'Invalid number of courses',
-                'number_clubs' => 'Invalid number of clubs',
+                $this->MAJOR => 'Invalid Entry',
+                $this->GPA => 'Invalid Entry',
+                $this->NUM_COURSES => 'Invalid Entry',
+                $this->NUM_CLUBS => 'Invalid Entry'
             ]));
         }
 
-        return $valid;
+        return false;
     }
 
 }
