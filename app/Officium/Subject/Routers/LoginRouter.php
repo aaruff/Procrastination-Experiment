35954db70cc45b4f55d59beb1550935050e248a7
Anwar A. Ruff <anwarruff@gmail.com>
2015-04-06 18:07:41 -0400

    Added doc blocks

--- app/Officium/Subject/Routers/LoginRouter.php
+++ app/Officium/Subject/Routers/LoginRouter.php
@@ -5,18 +5,35 @@ namespace Officium\Subject\Routers;
 
 use Officium\Framework\Routers\Router;
 
+/**
+ * Class LoginRouter
+ * @package Officium\Subject\Routers
+ */
 class LoginRouter extends Router
 {
+    /**
+     * Returns the login uri
+     * @return string
+     */
     public static function uri()
     {
         return '/login';
     }
 
+    /**
+     * Returns the route to the template
+     * @return string
+     */
     public static function getTemplate()
     {
         return '/pages/subject/login.twig';
     }
 
+    /**
+     * Returns the controller route.
+     * @param string $method
+     * @return string
+     */
     public static function controllerRoute($method = '')
     {
         if ($method == self::$POST) {
