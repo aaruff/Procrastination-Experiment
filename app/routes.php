98e0152362f866a74ae5b5893e4744651908de40
Anwar A. Ruff <anwarruff@gmail.com>
2015-04-08 17:01:01 -0400

    Substituted string route specification with resource map requests.

--- app/routes.php
+++ app/routes.php
@@ -1,6 +1,8 @@
 <?php
 use \Officium\User\Maps\LoginMap;
 use \Officium\Subject\Maps\SurveyMap;
+use \Officium\Experimenter\Maps\DashboardMap;
+use \Officium\Experimenter\Maps\SessionMap;
 //---------------------------------------------------
 // Subject Routes
 //---------------------------------------------------
@@ -15,10 +17,8 @@ Route::post(SurveyMap::toUri(), SurveyMap::toController('POST'));
 //---------------------------------------------------
 // Experimenter Routes
 //---------------------------------------------------
-Route::get('/experimenter/login', '\Officium\Controllers\Experimenter\ExperimenterLoginController:get');
-Route::post('/experimenter/login', 'Officium\Controllers\Experimenter\ExperimenterLoginController:post');
-Route::get('/experiment/dashboard', 'Officium\Controllers\Experimenter\Experiment\DashboardController:get');
-Route::post('/experiment/dashboard', 'Officium\Controllers\Experimenter\Experiment\DashboardController:post');
+Route::get(DashboardMap::toUri(), DashboardMap::toController());
+Route::post(DashboardMap::toUri(), DashboardMap::toController('POST'));
 Route::get('/experiment/session/:id', 'Officium\Controllers\Experimenter\Experiment\SessionController:show');
-Route::get('/experiment/session/', 'Officium\Controllers\Experimenter\Experiment\SessionController:get');
+Route::get(SessionMap::toUri(), SessionMap::toController());
 Route::post('/experiment/session/create', 'Officium\Controllers\Experimenter\Experiment\SessionController:create');
