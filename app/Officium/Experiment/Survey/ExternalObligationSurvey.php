1d5f2809eb566709d90779950fd0cb74a9cb7fca
Anwar A. Ruff <anwarruff@gmail.com>
2015-04-10 18:39:56 -0400

    Factored out validator

--- app/Officium/Experiment/Survey/ExternalObligationSurvey.php
+++ app/Officium/Experiment/Survey/ExternalObligationSurvey.php
@@ -2,32 +2,11 @@
 
 namespace Officium\Experiment\Survey;
 
-use Officium\Framework\Models\FormModel;
-use Respect\Validation\Exceptions\ValidationExceptionInterface as ValidationException;
-use Respect\Validation\Validator as Validator;
-
-class ExternalObligationSurvey extends FormModel
+class ExternalObligationSurvey
 {
     public function __construct($entries)
     {
         $keys = [];
         parent::__construct($keys, $entries);
     }
-
-    /**
-     * Validates general academic form entries.
-     *
-     * @return array
-     */
-    public function validate()
-    {
-        $errorMessages = [];
-
-        if ( ! Valdiator::int()->notEmpty()->between(0, 1000)) {
-            $errorMessages['hours_course_work'] = 'Invalid number of hours.';
-        }
-
-        return $errorMessages;
-    }
-
 }
