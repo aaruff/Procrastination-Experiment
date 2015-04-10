ebfc50e1705243d5f6bce1ddcbaaac3668296a37
Anwar A. Ruff <anwarruff@gmail.com>
2015-04-10 15:30:21 -0400

    Updated imports

--- app/Officium/Framework/Controllers/DashboardController.php
+++ app/Officium/Framework/Controllers/DashboardController.php
@@ -1,6 +1,9 @@
 <?php
 namespace Officium\Framework\Controllers;
 
+use Officium\Experiment\Treatment;
+use Officium\Framework\Maps\DashboardMap;
+
 class DashboardController extends BaseController
 {
     public function  __construct()
