1388ec9b8df814714b195f253c93ee77afdf04e3
Anwar A. Ruff <anwarruff@gmail.com>
2015-04-10 18:45:32 -0400

    Created the time limit validator class

---
+++ app/Officium/Framework/Validators/TimeLimitValidator.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Officium\Framework\Validators;
+
+
+class TimeLimitValidator extends Validator
+{
+
+    /**
+     * Validates the entry provided.
+     * @param $entry
+     * @return boolean
+     */
+    public function validate($entry)
+    {
+        // TODO: Implement validate() method.
+    }
+}
