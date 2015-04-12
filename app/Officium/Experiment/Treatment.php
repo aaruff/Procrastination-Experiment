8ffc8e7593fc97d62d90e9c1f26fea0a1e8c9ede
Anwar A. Ruff <anwarruff@gmail.com>
2015-04-11 22:58:30 -0400

    Moved validation to the TreatmentForm

--- app/Officium/Experiment/Treatment.php
+++ app/Officium/Experiment/Treatment.php
@@ -12,11 +12,16 @@ class Treatment extends Model
 {
     public $timestamps = false;
 
-    private static $treatmentTypes = ['task:1', 'task:3', 'task:3_timeLimit_penalty_adjustableDeadline'];
+    private static $treatmentType = 'task:3_timeLimit_penalty_adjustableDeadline';
 
     protected $table = 'treatments';
 
     protected $fillable = ['type'];
+
+    public static function getTypes()
+    {
+        return self::$treatmentType;
+    }
 
     /**
      * @param $dateTime
@@ -66,41 +71,5 @@ class Treatment extends Model
         $session = Treatment::where('id', '=', $id)->first();
 
         return $session;
-    }
-
-    /**
-     * Returns error messages if the validation failed.
-     *
-     * @param $entries
-     * @return string errorMessages
-     */
-    public static function validate($entries)
-    {
-        $errorMessages = [];
-        if (isset($entries['type']) && v::string()->notEmpty()->validate($entries['type']) && ! in_array($entries['type'], self::$treatmentTypes)) {
-           $errorMessages['general'] = 'Invalid form submission.';
-        }
-
-        // Validate input
-        try {
-            v::arr()
-                ->key('subject_deadline_enabled', v::yes())
-                ->key('size', v::notEmpty()->int()->between(1,100, true))
-                ->key('first_task_deadline', v::notEmpty()->date('m-d-Y H:i:s'))
-                ->key('second_task_deadline', v::notEmpty()->date('m-d-Y H:i:s'))
-                ->key('task_three_deadline', v::notEmpty()->date('m-d-Y H:i:s'))
-                ->key('time_limit', v::notEmpty()->int()->between(1,1000, true))
-                ->key('payoff', v::notEmpty()->int()->between(1,1000, true))
-                ->key('penalty', v::notEmpty()->numeric()->between(0,1000, true))
-                ->assert($entries);
-        }
-            // Handle authentication errors
-        catch (ValidationException $e) {
-            $errorMessages = array_filter($e->findMessages(
-                ['size', 'first_task_deadline', 'second_task_deadline', 'third_task_deadline',
-                    'time_limit', 'payoff', 'penalty', 'subject_deadline_enabled']));
-        }
-
-        return $errorMessages;
     }
 }
