cfb60e379d14df6e3979a1499a3db1dbaf912ebb
Anwar A. Ruff <anwarruff@gmail.com>
2015-04-13 14:41:43 -0400

    Implemented the validator

--- app/Officium/Framework/Validators/CheckboxValidator.php
+++ app/Officium/Framework/Validators/CheckboxValidator.php
@@ -3,12 +3,32 @@
 namespace Officium\Framework\Validators;
 
 use Respect\Validation\Validator as v;
+use Respect\Validation\Exceptions\NestedValidationExceptionInterface;
 
 class CheckboxValidator extends Validator
 {
+    private $required;
+
+    public function __construct($required = false)
+    {
+        $this->required = $required;
+    }
+
     public function validate($item)
     {
-        // TODO: Implement with exceptions
-        return v::when(v::notEmpty(), v::true())->validate($item);
+        try {
+            if ( ! $this->required) {
+                v::when(v::notEmpty(), v::true())->assert($item);
+            }
+            else {
+                v::notEmpty()->true()->assert($item);
+            }
+            return true;
+        }
+        catch(NestedValidationExceptionInterface $e) {
+            $this->setError($e->getFullMessage());
+        }
+
+        return false;
     }
 }
