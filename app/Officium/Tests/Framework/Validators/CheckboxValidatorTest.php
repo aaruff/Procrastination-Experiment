a35caefe8b21ba846a5340ceba992b985d8125bd
Anwar A. Ruff <anwarruff@gmail.com>
2015-04-13 14:44:41 -0400

    Created the Checkbox Validator unit test

---
+++ app/Officium/Tests/Framework/Validators/CheckboxValidatorTest.php
@@ -0,0 +1,28 @@
+<?php
+namespace Framework\Validators;
+
+use Officium\Experiment\Treatment;
+use Officium\Framework\Validators\CheckboxValidator;
+
+class CheckboxValidatorTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     */
+    public function Given_ValidEntryProvided_When_Validated_Should_ReturnTrue()
+    {
+        $checkboxValidator = new CheckboxValidator();
+        $this->assertTrue($checkboxValidator->validate('on'));
+        $this->assertTrue($checkboxValidator->validate(''));
+    }
+
+    /**
+     * @test
+     */
+    public function Given_InvalidEntryProvided_When_Validated_Should_ReturnTrue()
+    {
+        $checkboxValidator = new CheckboxValidator(true);
+        $this->assertFalse($checkboxValidator->validate('not valid'));
+        $this->assertFalse($checkboxValidator->validate(''));
+    }
+}
