abe795d3c9723820ae0533638c2c73eaae8c02f2
Anwar A. Ruff <anwarruff@gmail.com>
2015-04-14 14:29:29 -0400

    Created class

---
+++ app/Officium/Experiment/Treatment/ThreeTaskPenaltyRateTreatment.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Officium\Experiment\Treatment;
+
+use Experiment\Treatment\Task\TaskDeadline;
+use Experiment\Treatment\Task\TaskPenaltyRate;
+use Officium\Experiment\Subject;
+use Officium\Experiment\Treatment\Task\TaskTimeLimit;
+use Officium\Framework\Forms\ThreeTaskPenaltyRateForm;
+
+class ThreeTaskPenaltyRateTreatment 
+{
+    public static function create(ThreeTaskPenaltyRateForm $treatmentForm)
+    {
+        $treatmentId = Treatment::createTreatment($treatmentForm->getType());
+        Subject::createSubjects($treatmentForm->getNumberSubjects(), $treatmentId);
+        AlternateDeadlineTreatment::createTreatment($treatmentForm->getAlternateDeadlineOption(), $treatmentId);
+
+        $taskNumber = 1;
+        $deadlines = $treatmentForm->getTaskDeadlines();
+        foreach ($deadlines as $deadline) {
+            $taskId = Task::createTask($form->getType(), $taskNumber, $treatmentId, $treatmentForm->getPayoff());
+
+            TaskDeadline::createTaskDeadline($taskId, $deadline);
+            TaskTimeLimit::createTaskTimeLimit($taskId, $treatmentForm->getTaskTimeLimits());
+            TaskPenaltyRate::createPenaltyRate($taskId, $treatmentForm->getPenaltyRate());
+            ++$taskNumber;
+        }
+    }
+}
