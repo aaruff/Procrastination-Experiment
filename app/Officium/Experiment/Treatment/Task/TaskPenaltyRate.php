cfaba7e7e13b0e05de8a8ea102a97340ae0d3ecb
Anwar A. Ruff <anwarruff@gmail.com>
2015-04-14 13:55:38 -0400

    Added instantiation method

--- app/Officium/Experiment/Treatment/Task/TaskPenaltyRate.php
+++ app/Officium/Experiment/Treatment/Task/TaskPenaltyRate.php
@@ -15,4 +15,17 @@ class TaskPenaltyRate extends Model
      * @var string database table name
      */
     protected $table = 'task_penalty_rates';
+
+    /**
+     * @param $taskId
+     * @param $rate
+     */
+    public static function createPenaltyRate($taskId, $rate)
+    {
+        $penaltyRate = new TaskPenaltyRate();
+        $penaltyRate->task_id = $taskId;
+        $penaltyRate->rate = $rate;
+        $penaltyRate->save();
+    }
+
 }
