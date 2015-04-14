60bdf20721c679f7d605e3d0d6c2946bc67fa995
Anwar A. Ruff <anwarruff@gmail.com>
2015-04-14 14:30:16 -0400

    Implemented usage of ThreeTaskPenaltyRateForm and Builder

--- app/Officium/Framework/Controllers/SessionController.php
+++ app/Officium/Framework/Controllers/SessionController.php
@@ -2,10 +2,11 @@
 
 namespace Officium\Framework\Controllers;
 
+use Officium\Framework\Forms\ThreeTaskPenaltyRateForm;
 use Officium\Framework\Maps\DashboardMap;
 use Officium\Framework\Maps\SessionMap;
 use Officium\Experiment\Treatment;
-use Officium\Experiment\Subject;
+use Officium\Framework\Models\TreatmentBuilder;
 
 class SessionController extends BaseController
 {
@@ -27,25 +28,14 @@ class SessionController extends BaseController
      */
     public function post()
     {
-        $post = $this->request->post();
-
-        $errors = Treatment::validate($post);
-        if ( ! empty($errors)) {
-            $this->app->flash('errors', $errors);
+        $form = new ThreeTaskPenaltyRateForm($this->request->post());
+        if ($form->validate()) {
+            $this->app->flash('errors', $form->getErrors());
             $this->response->redirect(SessionMap::toUri());
             return;
         }
 
-        $session = Treatment::create($post['']);
-        $size = intval($post['size']);
-
-        for ($i = 0; $i < $size; ++$i) {
-            $subject = new Subject();
-            $subject->login = $subject->generateSubjectLoginName();
-            $subject->password = password_hash($session->id . $subject->login, PASSWORD_DEFAULT);
-            $subject->session_id = $session->id;
-            $subject->save();
-        }
+        TreatmentBuilder::make($form);
         $this->app->redirect(DashboardMap::toUri());
     }
 
