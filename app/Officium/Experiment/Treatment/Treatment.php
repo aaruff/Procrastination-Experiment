9882f2834058861acdd10ee32385574633e9cee5
Anwar A. Ruff <anwarruff@gmail.com>
2015-04-14 13:55:53 -0400

    Added instantiation method

--- app/Officium/Experiment/Treatment/Treatment.php
+++ app/Officium/Experiment/Treatment/Treatment.php
@@ -16,4 +16,19 @@ class Treatment extends Model
      * @var string database table name
      */
     protected $table = 'treatments';
+
+    /**
+     * Creates a treatment and returns its ID.
+     *
+     * @param $type
+     * @return mixed
+     */
+    public static function createTreatment($type)
+    {
+        $treatment = new Treatment();
+        $treatment->type = $type;
+        $treatment->save();
+
+        return $treatment->id;
+    }
 }
