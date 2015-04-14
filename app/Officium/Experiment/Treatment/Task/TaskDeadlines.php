797738fd18321f2115dbb2bac0894d16eb0bcf4b
Anwar A. Ruff <anwarruff@gmail.com>
2015-04-14 12:10:49 -0400

    Implemented required properties

--- app/Officium/Experiment/Treatment/Task/TaskDeadlines.php
+++ app/Officium/Experiment/Treatment/Task/TaskDeadlines.php
@@ -5,5 +5,14 @@ namespace Experiment\Treatment\Task;
 
 class TaskDeadlines
 {
+    /**
+     * @var bool database timestamp enabled
+     */
+    public $timestamps = false;
+
+    /**
+     * @var string database table name
+     */
+    protected $table = 'task_deadlines';
 
 }
