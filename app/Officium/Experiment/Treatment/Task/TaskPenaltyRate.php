98ec59d15bc672daf686af1564ca4f56a686b701
Anwar A. Ruff <anwarruff@gmail.com>
2015-04-14 12:10:59 -0400

    Implemented required properties

--- app/Officium/Experiment/Treatment/Task/TaskPenaltyRate.php
+++ app/Officium/Experiment/Treatment/Task/TaskPenaltyRate.php
@@ -5,5 +5,13 @@ namespace Experiment\Treatment\Task;
 
 class TaskPenaltyRate
 {
+    /**
+     * @var bool database timestamp enabled
+     */
+    public $timestamps = false;
 
+    /**
+     * @var string database table name
+     */
+    protected $table = 'task_penalty_rates';
 }
