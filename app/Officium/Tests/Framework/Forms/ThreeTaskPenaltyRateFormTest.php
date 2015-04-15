99718474ee0a53ca85d59a4382b98bf2cd22719b
Anwar A. Ruff <anwarruff@gmail.com>
2015-04-15 13:27:58 -0400

    Added tests for new functions

--- app/Officium/Tests/Framework/Forms/ThreeTaskPenaltyRateFormTest.php
+++ app/Officium/Tests/Framework/Forms/ThreeTaskPenaltyRateFormTest.php
@@ -2,7 +2,6 @@
 namespace Framework\Forms;
 
 
-use Officium\Framework\HTTP\PostFilter;
 use Officium\Framework\Forms\ThreeTaskPenaltyRateForm;
 
 class ThreeTaskPenaltyRateFormTest extends \PHPUnit_Framework_TestCase
@@ -25,6 +24,9 @@ class ThreeTaskPenaltyRateFormTest extends \PHPUnit_Framework_TestCase
 
         $form = new ThreeTaskPenaltyRateForm();
         $this->assertTrue($form->validate($post));
+
+        $formTwo = new ThreeTaskPenaltyRateForm($post);
+        $this->assertTrue($formTwo->validate());
     }
 
     /**
@@ -47,7 +49,53 @@ class ThreeTaskPenaltyRateFormTest extends \PHPUnit_Framework_TestCase
 
         $form = new ThreeTaskPenaltyRateForm();
         foreach ($posts as $post) {
-            $this->assertFalse($form->validate(PostFilter::filterEntries($post, $form->getFormKeys())));
+            $this->assertFalse($form->validate($post, $form->getFormKeys()));
         }
+    }
+
+    public function Given_TaskDeadlinesSet_When_GetterCalled_Should_ReturnDeadlinesArray()
+    {
+        $expect = ['12-01-2015 10:00 am', '12-02-2015 10:00 am', '12-03-2015 10:00 am'];
+        $post = [
+            ThreeTaskPenaltyRateForm::$TASK_ONE_DEADLINE_KEY => $expect[0],
+            ThreeTaskPenaltyRateForm::$TASK_TWO_DEADLINE_KEY => $expect[1],
+            ThreeTaskPenaltyRateForm::$TASK_THREE_DEADLINE_KEY => $expect[2]
+        ];
+
+        $form = new ThreeTaskPenaltyRateForm($post);
+        $this->assertEquals($expect, $form->getEntries());
+    }
+
+    public function Given_AlternateDeadlineEnabled_When_GetterCalled_Should_ReturnTrue()
+    {
+        $post = [
+            ThreeTaskPenaltyRateForm::$NUMBER_SUBJECTS_KEY => '1',
+            ThreeTaskPenaltyRateForm::$ALTERNATE_TASK_DEADLINE_KEY => 'on',
+            ThreeTaskPenaltyRateForm::$PENALTY_RATE_KEY => '0.5',
+            ThreeTaskPenaltyRateForm::$TASK_ONE_DEADLINE_KEY => '12-01-2015 10:00 am',
+            ThreeTaskPenaltyRateForm::$TASK_TWO_DEADLINE_KEY => '12-02-2015 10:00 am',
+            ThreeTaskPenaltyRateForm::$TASK_THREE_DEADLINE_KEY => '12-03-2015 10:00 am',
+            ThreeTaskPenaltyRateForm::$PAYOFF_KEY => '12',
+            ThreeTaskPenaltyRateForm::$TASK_TIME_LIMIT_KEY => '60'
+        ];
+
+        $form = new ThreeTaskPenaltyRateForm($post);
+        $this->assertTrue($form->getAlternateDeadlineOption());
+    }
+
+    public function Given_AlternateDeadlineNotProvided_When_GetterCalled_Should_ReturnFalse()
+    {
+        $post = [
+            ThreeTaskPenaltyRateForm::$NUMBER_SUBJECTS_KEY => '1',
+            ThreeTaskPenaltyRateForm::$PENALTY_RATE_KEY => '0.5',
+            ThreeTaskPenaltyRateForm::$TASK_ONE_DEADLINE_KEY => '12-01-2015 10:00 am',
+            ThreeTaskPenaltyRateForm::$TASK_TWO_DEADLINE_KEY => '12-02-2015 10:00 am',
+            ThreeTaskPenaltyRateForm::$TASK_THREE_DEADLINE_KEY => '12-03-2015 10:00 am',
+            ThreeTaskPenaltyRateForm::$PAYOFF_KEY => '12',
+            ThreeTaskPenaltyRateForm::$TASK_TIME_LIMIT_KEY => '60'
+        ];
+
+        $form = new ThreeTaskPenaltyRateForm($post);
+        $this->assertFalse($form->getAlternateDeadlineOption());
     }
 }
