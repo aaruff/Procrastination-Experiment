ae596aba0daf82bdacb215b14078a659e64e1d37
Anwar A. Ruff <anwarruff@gmail.com>
2015-04-15 18:58:58 -0400

    Added clearErrors() and static error message properties

--- app/Officium/Framework/Validators/Validator.php
+++ app/Officium/Framework/Validators/Validator.php
@@ -9,7 +9,17 @@ namespace Officium\Framework\Validators;
  */
 abstract class Validator
 {
-    protected $error = '';
+    protected static $NOT_EMPTY = 'This field is required.';
+    protected static $TRUE = 'This field must be either selected or unselected.';
+    protected static $DATE_TIME = 'This field requires a validly formatted Date and Time.';
+    protected static $STRING = 'This field must consist of only text.';
+    protected static $FLOAT = 'This field must be a floating point number.';
+    protected static $INTEGER = 'This field must be an integer.';
+
+    /**
+     * @var string[]
+     */
+    protected $errors = [];
 
     /**
      * Validates the entry provided.
@@ -20,19 +30,34 @@ abstract class Validator
 
     /**
      * Sets the error message.
-     * @param $error
+     * @param $errors
      */
-    public function setError($error)
+    public function setErrors($errors)
     {
-        $this->error = $error;
+        $this->errors = $errors;
     }
 
     /**
      * Returns the error message.
      * @return mixed
      */
-    public function getError()
+    public function getErrors()
     {
-        return $this->error;
+        $parsedErrors = [];
+        foreach ($this->errors as $error) {
+            if ( ! empty($error)) {
+                $parsedErrors[] = $error;
+            }
+        }
+
+        return $parsedErrors;
+    }
+
+    /**
+     * Clears any errors that might be stored from a previous validation.
+     */
+    public function clearErrors()
+    {
+        $this->errors = [];
     }
 }
