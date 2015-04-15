24b1fe752d56af08fb4ce7cfe87bbd3c315c4f88
Anwar A. Ruff <anwarruff@gmail.com>
2015-04-15 17:15:27 -0400

    Added validate stub

--- app/Officium/Experiment/Survey/ExternalObligationSurvey.php
+++ app/Officium/Experiment/Survey/ExternalObligationSurvey.php
@@ -2,11 +2,23 @@
 
 namespace Officium\Experiment\Survey;
 
-class ExternalObligationSurvey
+use Officium\Framework\Models\FormModel;
+
+class ExternalObligationSurvey extends FormModel
 {
     public function __construct($entries)
     {
         $keys = [];
         parent::__construct($keys, $entries);
+    }
+
+    /**
+     * Validates the survey against the provided entries.
+     *
+     * @return boolean
+     */
+    public function validate()
+    {
+        // TODO: Implement validate() method.
     }
 }
