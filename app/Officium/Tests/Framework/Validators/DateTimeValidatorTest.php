458a74436465d41150185f3932f0d75fa7c213d3
Anwar A. Ruff <anwarruff@gmail.com>
2015-04-16 15:21:25 -0400

    Added test cases

--- app/Officium/Tests/Framework/Validators/DateTimeValidatorTest.php
+++ app/Officium/Tests/Framework/Validators/DateTimeValidatorTest.php
@@ -15,6 +15,9 @@ class DateTimeValidatorTest extends \PHPUnit_Framework_TestCase {
     {
         $dtValidator = new DateTimeValidator();
         $this->assertTrue($dtValidator->validate('12-05-2015 12:00 pm'));
+        // Test days and times with leading zeros
+        $this->assertTrue($dtValidator->validate('04-16-2015 03:04 pm'));
+        $this->assertTrue($dtValidator->validate('04-06-2015 03:04 pm'));
     }
 
     /**
