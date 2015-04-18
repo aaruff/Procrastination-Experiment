2be0c2ee216e0ea4732a4f9bfd720856c67fd866
Anwar A. Ruff <anwarruff@gmail.com>
2015-04-17 23:25:59 -0400

    Added table relations

--- app/Officium/Experiment/Treatment/Treatment.php
+++ app/Officium/Experiment/Treatment/Treatment.php
@@ -2,8 +2,9 @@
 
 namespace Officium\Experiment\Treatment;
 
-
+use Officium\Experiment\Treatment\Task;
 use Illuminate\Database\Eloquent\Model;
+use Officium\Experiment\Subject;
 
 class Treatment extends Model
 {
@@ -18,15 +19,45 @@ class Treatment extends Model
     protected $table = 'treatments';
 
     /**
+     * @var string
+     */
+    public static $THREE_TASK_TIME_LIMIT_PENALTY_ADJUSTABLE_DEADLINE = 'task:3_timeLimit_penalty_adjustableDeadline';
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function subjects()
+    {
+        return $this->hasMany(get_class(new Subject()), 'treatment_id');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function alternateDeadlineTreatment()
+    {
+        return $this->hasOne(get_class(new AlternateDeadlineTreatment()), 'treatment_id');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function tasks()
+    {
+        return $this->hasMany(get_class(new Task()), 'treatment_id');
+    }
+
+    /**
      * Creates a treatment and returns its ID.
      *
      * @param $type
      * @return mixed
      */
-    public static function createTreatment($type)
+    public static function createTreatment($type, $size)
     {
         $treatment = new Treatment();
         $treatment->type = $type;
+        $treatment->size = $size;
         $treatment->save();
 
         return $treatment->id;
