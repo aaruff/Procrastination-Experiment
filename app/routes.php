131d701fc9c6ad5a485f669394a065d0e2d07544
Anwar A. Ruff <anwarruff@gmail.com>
2015-04-21 15:25:26 -0400

    Updated the maps

--- app/routes.php
+++ app/routes.php
@@ -1,26 +1,28 @@
 <?php
 
+use Officium\Framework\Maps\TreatmentMap;
 use Officium\Framework\Maps\SurveyMap;
 use Officium\Framework\Maps\LoginMap;
 use Officium\Framework\Maps\DashboardMap;
 use Officium\Framework\Maps\SessionMap;
+use Statical\SlimStatic\Route;
 
 //---------------------------------------------------
 // Subject Routes
 //---------------------------------------------------
 Route::get(LoginMap::toUri(), LoginMap::toController());
-Route::post(LoginMap::toUri(), LoginMap::toController('POST'));
+Route::post(LoginMap::toUri(), LoginMap::toController(LoginMap::$POST));
 
 // Incoming Survey
 Route::get(SurveyMap::toUri(), SurveyMap::toController());
-Route::post(SurveyMap::toUri(), SurveyMap::toController('POST'));
+Route::post(SurveyMap::toUri(), SurveyMap::toController(SurveyMap::$POST));
 
 
 //---------------------------------------------------
 // Experimenter Routes
 //---------------------------------------------------
 Route::get(DashboardMap::toUri(), DashboardMap::toController());
-Route::post(DashboardMap::toUri(), DashboardMap::toController('POST'));
+Route::post(DashboardMap::toUri(), DashboardMap::toController(DashboardMap::$POST));
 Route::get(SessionMap::toUri(), SessionMap::toController());
-Route::post(SessionMap::toUri(), SessionMap::toController('POST'));
-//Route::get('/experiment/session/:id', 'Officium\Controllers\Experimenter\Experiment\SessionController:show');
+Route::post(SessionMap::toUri(), SessionMap::toController(SessionMap::$POST));
+Route::get(TreatmentMap::toUri(TreatmentMap::$ID), TreatmentMap::toController());
