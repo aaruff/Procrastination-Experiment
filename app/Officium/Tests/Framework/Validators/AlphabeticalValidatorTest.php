0d36b54c53febf9004563fc2af2520760b3f0525
Anwar A. Ruff <anwarruff@gmail.com>
2015-04-24 11:32:43 -0400

    Created unit test

---
+++ app/Officium/Tests/Framework/Validators/AlphabeticalValidatorTest.php
@@ -0,0 +1,32 @@
+<?php
+
+
+namespace Framework\Validators;
+
+
+use Officium\Framework\Validators\AlphabeticalValidator;
+
+class AlphabeticalValidatorTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     */
+    public function Given_ValidAlphaStringProvided_When_Validated_Should_ReturnTrue()
+    {
+        $alphaValidator = new AlphabeticalValidator();
+        $this->assertTrue($alphaValidator->validate('as'));
+        $this->assertTrue($alphaValidator->validate('ai'));
+    }
+
+    /**
+     * @test
+     */
+    public function Given_InvalidAlphaStringProvided_When_Validated_Should_ReturnFalse()
+    {
+        $alphaValidator = new AlphabeticalValidator();
+        $this->assertFalse($alphaValidator->validate(''));
+        $this->assertFalse($alphaValidator->validate('1'));
+        $this->assertFalse($alphaValidator->validate(2));
+        $this->assertFalse($alphaValidator->validate('!#?'));
+    }
+}
