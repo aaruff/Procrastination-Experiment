7be217a88eb97e946bfeebf6911de7e8566d9b10
Anwar A. Ruff <anwarruff@gmail.com>
2015-04-24 11:49:21 -0400

    Added documentation

--- app/Officium/Framework/Presentations/Forms/LoginForm.php
+++ app/Officium/Framework/Presentations/Forms/LoginForm.php
@@ -6,9 +6,13 @@ namespace Officium\Framework\Presentations\Forms;
 use Officium\Framework\Validators\AlphabeticalValidator;
 use Officium\Framework\Validators\AlphaNumericValidator;
 
+/**
+ * This class stores and validates the login form data.
+ *
+ * @package Officium\Framework\Presentations\Forms
+ */
 class LoginForm extends Form
 {
-
     public static $LOGIN = 'login';
     public static $PASSWORD = 'password';
 
@@ -27,14 +31,21 @@ class LoginForm extends Form
         $validators = [];
         $validators[self::$LOGIN] = new AlphabeticalValidator();
         $validators[self::$PASSWORD] = new AlphaNumericValidator();
+        return $validators;
     }
 
+    /**
+     * @return string
+     */
     public function getLogin()
     {
         $entries = $this->getEntries();
         return $entries[self::$LOGIN];
     }
 
+    /**
+     * @return string
+     */
     public function getPassword()
     {
         $entries = $this->getEntries();
