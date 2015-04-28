51d96cd9cfbf78d3e6cfc054b6bf22c5311ff930
Anwar A. Ruff <anwarruff@gmail.com>
2015-04-28 15:49:12 -0400

    Deleted unused functions

--- app/Officium/Framework/Maps/SurveyMap.php
+++ app/Officium/Framework/Maps/SurveyMap.php
@@ -1,7 +1,9 @@
 <?php
 
 namespace Officium\Framework\Maps;
+
 use Officium\Framework\Controllers\SurveyController;
+use Officium\Framework\Models\Session;
 
 /**
  * Survey Resource Map
@@ -10,57 +12,19 @@ use Officium\Framework\Controllers\SurveyController;
  */
 class SurveyMap extends ResourceMap
 {
-    private static $templates = ['a' => 'academic.twig', 'ao' => 'academicObligations.twig', 'eo' => 'externalObligations.twig'];
-    private static $surveyOrder = ['a', 'ao', 'eo'];
-
-    public static function isSurveyId($id)
-    {
-        if (isset(self::$templates[$id])) {
-            return true;
-        }
-
-        return false;
-    }
+    private static $templates = ['academic.twig', 'academicObligations.twig', 'externalObligations.twig'];
 
     /**
-     * Returns the corresponding template route for the provided id if set, otherwise
-     * the first survey template route is returned.
-     *
-     * @param $id
      * @return string template route
      */
-    public static function toRoute($id)
+    public static function toTemplate($surveyId)
     {
-        $rootPath = '/pages/subject/survey/';
-        if (self::isSurveyId($id)) {
-            return $rootPath . self::$templates[$id];
-        }
-
-        return $rootPath . self::$templates['a'];
+        return '/pages/subject/survey/' . self::$templates[$surveyId];
     }
 
     /**
-     * Returns the next survey ID if it exists, otherwise null is returned.
-     *
-     * @param $id
-     * @return mixed survey ID or null
+     * @return string
      */
-    public static function getNextSurveyId($id = null)
-    {
-        if ( self::isSurveyId($id)) {
-            // Look for the next survey ID and return it
-            foreach (self::$surveyOrder as $index => $surveyKey) {
-                // Found the survey ID, and there are more surveys to be completed
-                if ($surveyKey == $id && $index + 1 < count(self::$surveyOrder)) {
-                    return self::$surveyOrder[$index + 1];
-                }
-            }
-        }
-
-        $firstSurvey = 1;
-        return self::$surveyOrder[$firstSurvey];
-    }
-
     public static function toUri()
     {
         return '/survey';
