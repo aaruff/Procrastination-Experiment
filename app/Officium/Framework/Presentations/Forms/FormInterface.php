9370f28900e9a069c43f155121b77dc79130b5a0
Anwar A. Ruff <anwarruff@gmail.com>
2015-04-28 19:22:43 -0400

    Created FormInterface

---
+++ app/Officium/Framework/Presentations/Forms/FormInterface.php
@@ -0,0 +1,48 @@
+<?php
+namespace Officium\Framework\Presentations\Forms;
+
+
+interface FormInterface {
+
+    /**
+     * Validates form entries.
+     *
+     * @param array $entries
+     * @return boolean
+     */
+    public function validate(array $entries);
+
+    /**
+     * Returns form entries.
+     *
+     * @return array
+     */
+    public function getEntries();
+
+    /**
+     * Returns form keys.
+     *
+     * @return array
+     */
+    public function getKeys();
+
+    /**
+     * Returns the form type.
+     *
+     * @return string
+     */
+    public function getType();
+
+    /**
+     * Returns the form validation errors.
+     * @return array
+     */
+    public function getErrors();
+
+    /**
+     * Returns form entries along with any errors that occurred.
+     *
+     * @return array
+     */
+    public function getEntriesWithErrors();
+}
