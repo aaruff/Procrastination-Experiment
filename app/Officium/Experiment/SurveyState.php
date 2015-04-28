a0c0a26180333a35b5f1b34dccb40adcb5a4c0a1
Anwar A. Ruff <anwarruff@gmail.com>
2015-04-28 19:26:08 -0400

    Added getNextSurveyId func

--- app/Officium/Experiment/SurveyState.php
+++ app/Officium/Experiment/SurveyState.php
@@ -8,13 +8,25 @@ abstract class SurveyState
     const GENERAL = 0;
     const ACADEMIC_OBLIGATION = 1;
     const EXTERNAL_OBLIGATION = 2;
+    const COMPLETED = -1;
 
-    public static function getNextState($id)
+    /**
+     * Returns the next state ID.
+     *
+     * @param $id
+     * @return int
+     */
+    public static function getNextSurveyId($id)
     {
-        if ($id >= 0 && $id < SurveyState::EXTERNAL_OBLIGATION) {
+        if ($id >= 0 && $id < self::EXTERNAL_OBLIGATION) {
             return $id + 1;
         }
 
-        return 0;
+        return self::COMPLETED;
+    }
+
+    public static function isComplete($id)
+    {
+        return $id == self::COMPLETED;
     }
 }
