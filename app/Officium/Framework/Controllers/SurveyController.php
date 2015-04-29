a8902664c3dc3b7db54c3ccaa6c77bc3ac515915
Anwar A. Ruff <anwarruff@gmail.com>
2015-04-29 12:03:55 -0400

    Minor refactoring

--- app/Officium/Framework/Controllers/SurveyController.php
+++ app/Officium/Framework/Controllers/SurveyController.php
@@ -1,7 +1,7 @@
 <?php
 namespace Officium\Framework\Controllers;
 
-use Officium\Experiment\SurveyState;
+use Officium\Experiment\IncomingSurveyState;
 use Officium\Framework\Maps\SurveyMap;
 use Officium\Framework\Models\Session;
 use Officium\Framework\Presentations\Forms\SurveyFormFactory;
@@ -15,7 +15,7 @@ class SurveyController
     public function get()
     {
         $app = Slim::getInstance();
-        $app->render(SurveyMap::toTemplate(Session::getSurveyState()), $app->flashData());
+        $app->render(SurveyMap::toTemplate(), $app->flashData());
     }
 
     /**
@@ -25,16 +25,16 @@ class SurveyController
     {
         $app = Slim::getInstance();
 
-        $surveyForm = SurveyFormFactory::make(Session::getSurveyState());
+        $surveyForm = SurveyFormFactory::make();
         if ($surveyForm->validate($app->request->post())) {
             $surveyForm->saveToSession();
 
-            if (SurveyState::isComplete(Session::getSurveyState())) {
+            IncomingSurveyState::moveToNextSurvey();
+            if (IncomingSurveyState::isSurveyComplete()) {
                 var_dump(Session::getAllSurveyFormEntries());
                 return;
             }
 
-            Session::setSurveyId(SurveyState::getNextSurveyId(Session::getSurveyState()));
             $app->redirect(SurveyMap::toUri());
         }
         else {
