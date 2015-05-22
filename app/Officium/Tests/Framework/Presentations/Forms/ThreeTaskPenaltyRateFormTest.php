11c09f2320a1a44524e2219fd21041ace67981b4
Anwar A. Ruff <anwarruff@gmail.com>
2015-05-22 18:06:15 -0400

    Refactored for renamed class

--- app/Officium/Tests/Framework/Presentations/Forms/ThreeTaskPenaltyRateFormTest.php
+++ app/Officium/Tests/Framework/Presentations/Forms/ThreeTaskPenaltyRateFormTest.php
@@ -2,7 +2,7 @@
 namespace Framework\Forms;
 
 
-use Officium\Framework\Presentations\Forms\SessionForm;
+use Officium\Framework\View\Forms\ThreeTaskPenaltyTreatmentForm;
 
 class ThreeTaskPenaltyRateFormTest extends \PHPUnit_Framework_TestCase
 {
@@ -12,21 +12,21 @@ class ThreeTaskPenaltyRateFormTest extends \PHPUnit_Framework_TestCase
     public function Given_ValidEntriesProvided_When_Validated_Should_ReturnTrue()
     {
         $post = [
-            SessionForm::$SIZE => '1',
-            SessionForm::$ADJUSTABLE_DEADLINE => 'on',
-            SessionForm::$PENALTY_RATE => '0.5',
-            SessionForm::$TASK_ONE_DEADLINE => '12-01-2015 10:00 am',
-            SessionForm::$TASK_TWO_DEADLINE => '12-02-2015 10:00 am',
-            SessionForm::$TASK_THREE_DEADLINE => '12-03-2015 10:00 am',
-            SessionForm::$PAYOFF => '12',
-            SessionForm::$TASK_TIME_LIMIT => '60'
+            \Officium\Framework\View\Forms\ThreeTaskPenaltyTreatmentForm::$SIZE => '1',
+            \Officium\Framework\View\Forms\ThreeTaskPenaltyTreatmentForm::$ADJUSTABLE_DEADLINE => 'on',
+            \Officium\Framework\View\Forms\ThreeTaskPenaltyTreatmentForm::$PENALTY_RATE => '0.5',
+            ThreeTaskPenaltyTreatmentForm::$TASK_ONE_DEADLINE => '12-01-2015 10:00 am',
+            ThreeTaskPenaltyTreatmentForm::$TASK_TWO_DEADLINE => '12-02-2015 10:00 am',
+            \Officium\Framework\View\Forms\ThreeTaskPenaltyTreatmentForm::$TASK_THREE_DEADLINE => '12-03-2015 10:00 am',
+            ThreeTaskPenaltyTreatmentForm::$PAYOFF => '12',
+            \Officium\Framework\View\Forms\ThreeTaskPenaltyTreatmentForm::$TASK_TIME_LIMIT => '60'
         ];
 
-        $form = new SessionForm();
+        $form = new \Officium\Framework\View\Forms\ThreeTaskPenaltyTreatmentForm();
         $this->assertTrue($form->validate($post));
 
-        unset($post[SessionForm::$ADJUSTABLE_DEADLINE]);
-        $formTwo = new SessionForm($post);
+        unset($post[ThreeTaskPenaltyTreatmentForm::$ADJUSTABLE_DEADLINE]);
+        $formTwo = new \Officium\Framework\View\Forms\ThreeTaskPenaltyTreatmentForm($post);
         $this->assertTrue($formTwo->validate());
     }
 
@@ -37,53 +37,53 @@ class ThreeTaskPenaltyRateFormTest extends \PHPUnit_Framework_TestCase
     {
         $posts = [
             [
-                SessionForm::$SIZE => '1',
+                ThreeTaskPenaltyTreatmentForm::$SIZE => '1',
             ],
             [
-                SessionForm::$SIZE => '1',
-                SessionForm::$TASK_ONE_DEADLINE => '12-01-2015 10:00 am',
+                ThreeTaskPenaltyTreatmentForm::$SIZE => '1',
+                \Officium\Framework\View\Forms\ThreeTaskPenaltyTreatmentForm::$TASK_ONE_DEADLINE => '12-01-2015 10:00 am',
             ],
             [
-                SessionForm::$SIZE => '1',
-                SessionForm::$TASK_ONE_DEADLINE => '12-01-2015 10:00 am',
-                SessionForm::$TASK_TWO_DEADLINE => '12-02-2015 10:00 am',
-                SessionForm::$TASK_THREE_DEADLINE => '12-03-2015 10:00 am',
+                \Officium\Framework\View\Forms\ThreeTaskPenaltyTreatmentForm::$SIZE => '1',
+                ThreeTaskPenaltyTreatmentForm::$TASK_ONE_DEADLINE => '12-01-2015 10:00 am',
+                ThreeTaskPenaltyTreatmentForm::$TASK_TWO_DEADLINE => '12-02-2015 10:00 am',
+                \Officium\Framework\View\Forms\ThreeTaskPenaltyTreatmentForm::$TASK_THREE_DEADLINE => '12-03-2015 10:00 am',
             ],
             [
-                SessionForm::$SIZE => '1',
-                SessionForm::$TASK_ONE_DEADLINE => '12-01-2015 10:00 am',
-                SessionForm::$TASK_TWO_DEADLINE => '12-02-2015 10:00 am',
-                SessionForm::$TASK_THREE_DEADLINE => '12-03-2015 10:00 am',
-                SessionForm::$ADJUSTABLE_DEADLINE => 'on',
+                ThreeTaskPenaltyTreatmentForm::$SIZE => '1',
+                ThreeTaskPenaltyTreatmentForm::$TASK_ONE_DEADLINE => '12-01-2015 10:00 am',
+                ThreeTaskPenaltyTreatmentForm::$TASK_TWO_DEADLINE => '12-02-2015 10:00 am',
+                ThreeTaskPenaltyTreatmentForm::$TASK_THREE_DEADLINE => '12-03-2015 10:00 am',
+                \Officium\Framework\View\Forms\ThreeTaskPenaltyTreatmentForm::$ADJUSTABLE_DEADLINE => 'on',
             ],
             [
-                SessionForm::$SIZE => '1',
-                SessionForm::$TASK_ONE_DEADLINE => '12-01-2015 10:00 am',
-                SessionForm::$TASK_TWO_DEADLINE => '12-02-2015 10:00 am',
-                SessionForm::$TASK_THREE_DEADLINE => '12-03-2015 10:00 am',
-                SessionForm::$ADJUSTABLE_DEADLINE => 'on',
-                SessionForm::$PENALTY_RATE => '0.5',
+                \Officium\Framework\View\Forms\ThreeTaskPenaltyTreatmentForm::$SIZE => '1',
+                ThreeTaskPenaltyTreatmentForm::$TASK_ONE_DEADLINE => '12-01-2015 10:00 am',
+                ThreeTaskPenaltyTreatmentForm::$TASK_TWO_DEADLINE => '12-02-2015 10:00 am',
+                ThreeTaskPenaltyTreatmentForm::$TASK_THREE_DEADLINE => '12-03-2015 10:00 am',
+                \Officium\Framework\View\Forms\ThreeTaskPenaltyTreatmentForm::$ADJUSTABLE_DEADLINE => 'on',
+                ThreeTaskPenaltyTreatmentForm::$PENALTY_RATE => '0.5',
             ],
             [
-                SessionForm::$SIZE => '1',
-                SessionForm::$TASK_ONE_DEADLINE => '12-01-2015 10:00 am',
-                SessionForm::$TASK_TWO_DEADLINE => '12-02-2015 10:00 am',
-                SessionForm::$TASK_THREE_DEADLINE => '12-03-2015 10:00 am',
-                SessionForm::$ADJUSTABLE_DEADLINE => 'on',
-                SessionForm::$PENALTY_RATE => '0.5',
-                SessionForm::$PAYOFF => '12',
+                ThreeTaskPenaltyTreatmentForm::$SIZE => '1',
+                ThreeTaskPenaltyTreatmentForm::$TASK_ONE_DEADLINE => '12-01-2015 10:00 am',
+                ThreeTaskPenaltyTreatmentForm::$TASK_TWO_DEADLINE => '12-02-2015 10:00 am',
+                ThreeTaskPenaltyTreatmentForm::$TASK_THREE_DEADLINE => '12-03-2015 10:00 am',
+                \Officium\Framework\View\Forms\ThreeTaskPenaltyTreatmentForm::$ADJUSTABLE_DEADLINE => 'on',
+                ThreeTaskPenaltyTreatmentForm::$PENALTY_RATE => '0.5',
+                ThreeTaskPenaltyTreatmentForm::$PAYOFF => '12',
             ],
             [
-                SessionForm::$TASK_ONE_DEADLINE => '12-01-2015 10:00 am',
-                SessionForm::$TASK_TWO_DEADLINE => '12-02-2015 10:00 am',
-                SessionForm::$TASK_THREE_DEADLINE => '12-03-2015 10:00 am',
-                SessionForm::$ADJUSTABLE_DEADLINE => 'on',
-                SessionForm::$PAYOFF => '12',
-                SessionForm::$TASK_TIME_LIMIT => '60'
+                ThreeTaskPenaltyTreatmentForm::$TASK_ONE_DEADLINE => '12-01-2015 10:00 am',
+                ThreeTaskPenaltyTreatmentForm::$TASK_TWO_DEADLINE => '12-02-2015 10:00 am',
+                ThreeTaskPenaltyTreatmentForm::$TASK_THREE_DEADLINE => '12-03-2015 10:00 am',
+                \Officium\Framework\View\Forms\ThreeTaskPenaltyTreatmentForm::$ADJUSTABLE_DEADLINE => 'on',
+                \Officium\Framework\View\Forms\ThreeTaskPenaltyTreatmentForm::$PAYOFF => '12',
+                ThreeTaskPenaltyTreatmentForm::$TASK_TIME_LIMIT => '60'
             ],
         ];
 
-        $form = new SessionForm();
+        $form = new \Officium\Framework\View\Forms\ThreeTaskPenaltyTreatmentForm();
         foreach ($posts as $post) {
             $this->assertFalse($form->validate($post));
         }
@@ -95,14 +95,14 @@ class ThreeTaskPenaltyRateFormTest extends \PHPUnit_Framework_TestCase
     public function Given_InvalidEntriesProvided_When_Validated_Should_ReturnErrors()
     {
         $post = [
-            SessionForm::$SIZE => '1',
-            SessionForm::$ADJUSTABLE_DEADLINE => 'on',
-            SessionForm::$PENALTY_RATE => '0.5',
-            SessionForm::$PAYOFF => '12',
-            SessionForm::$TASK_TIME_LIMIT => '60'
+            ThreeTaskPenaltyTreatmentForm::$SIZE => '1',
+            ThreeTaskPenaltyTreatmentForm::$ADJUSTABLE_DEADLINE => 'on',
+            ThreeTaskPenaltyTreatmentForm::$PENALTY_RATE => '0.5',
+            ThreeTaskPenaltyTreatmentForm::$PAYOFF => '12',
+            ThreeTaskPenaltyTreatmentForm::$TASK_TIME_LIMIT => '60'
         ];
 
-        $form = new SessionForm();
+        $form = new \Officium\Framework\View\Forms\ThreeTaskPenaltyTreatmentForm();
         $form->validate($post);
         $this->assertTrue( ! empty($form->getErrors()));
     }
@@ -111,45 +111,45 @@ class ThreeTaskPenaltyRateFormTest extends \PHPUnit_Framework_TestCase
     {
         $expect = ['12-01-2015 10:00 am', '12-02-2015 10:00 am', '12-03-2015 10:00 am'];
         $post = [
-            SessionForm::$TASK_ONE_DEADLINE => $expect[0],
-            SessionForm::$TASK_TWO_DEADLINE => $expect[1],
-            SessionForm::$TASK_THREE_DEADLINE => $expect[2]
+            \Officium\Framework\View\Forms\ThreeTaskPenaltyTreatmentForm::$TASK_ONE_DEADLINE => $expect[0],
+            \Officium\Framework\View\Forms\ThreeTaskPenaltyTreatmentForm::$TASK_TWO_DEADLINE => $expect[1],
+            \Officium\Framework\View\Forms\ThreeTaskPenaltyTreatmentForm::$TASK_THREE_DEADLINE => $expect[2]
         ];
 
-        $form = new SessionForm($post);
+        $form = new \Officium\Framework\View\Forms\ThreeTaskPenaltyTreatmentForm($post);
         $this->assertEquals($expect, $form->getEntries());
     }
 
     public function Given_AlternateDeadlineEnabled_When_GetterCalled_Should_ReturnTrue()
     {
         $post = [
-            SessionForm::$SIZE => '1',
-            SessionForm::$ADJUSTABLE_DEADLINE => 'on',
-            SessionForm::$PENALTY_RATE => '0.5',
-            SessionForm::$TASK_ONE_DEADLINE => '12-01-2015 10:00 am',
-            SessionForm::$TASK_TWO_DEADLINE => '12-02-2015 10:00 am',
-            SessionForm::$TASK_THREE_DEADLINE => '12-03-2015 10:00 am',
-            SessionForm::$PAYOFF => '12',
-            SessionForm::$TASK_TIME_LIMIT => '60'
+            ThreeTaskPenaltyTreatmentForm::$SIZE => '1',
+            \Officium\Framework\View\Forms\ThreeTaskPenaltyTreatmentForm::$ADJUSTABLE_DEADLINE => 'on',
+            ThreeTaskPenaltyTreatmentForm::$PENALTY_RATE => '0.5',
+            ThreeTaskPenaltyTreatmentForm::$TASK_ONE_DEADLINE => '12-01-2015 10:00 am',
+            \Officium\Framework\View\Forms\ThreeTaskPenaltyTreatmentForm::$TASK_TWO_DEADLINE => '12-02-2015 10:00 am',
+            \Officium\Framework\View\Forms\ThreeTaskPenaltyTreatmentForm::$TASK_THREE_DEADLINE => '12-03-2015 10:00 am',
+            \Officium\Framework\View\Forms\ThreeTaskPenaltyTreatmentForm::$PAYOFF => '12',
+            \Officium\Framework\View\Forms\ThreeTaskPenaltyTreatmentForm::$TASK_TIME_LIMIT => '60'
         ];
 
-        $form = new SessionForm($post);
+        $form = new \Officium\Framework\View\Forms\ThreeTaskPenaltyTreatmentForm($post);
         $this->assertTrue($form->getSecondaryDeadlineEnabled());
     }
 
     public function Given_AlternateDeadlineNotProvided_When_GetterCalled_Should_ReturnFalse()
     {
         $post = [
-            SessionForm::$SIZE => '1',
-            SessionForm::$PENALTY_RATE => '0.5',
-            SessionForm::$TASK_ONE_DEADLINE => '12-01-2015 10:00 am',
-            SessionForm::$TASK_TWO_DEADLINE => '12-02-2015 10:00 am',
-            SessionForm::$TASK_THREE_DEADLINE => '12-03-2015 10:00 am',
-            SessionForm::$PAYOFF => '12',
-            SessionForm::$TASK_TIME_LIMIT => '60'
+            \Officium\Framework\View\Forms\ThreeTaskPenaltyTreatmentForm::$SIZE => '1',
+            \Officium\Framework\View\Forms\ThreeTaskPenaltyTreatmentForm::$PENALTY_RATE => '0.5',
+            \Officium\Framework\View\Forms\ThreeTaskPenaltyTreatmentForm::$TASK_ONE_DEADLINE => '12-01-2015 10:00 am',
+            ThreeTaskPenaltyTreatmentForm::$TASK_TWO_DEADLINE => '12-02-2015 10:00 am',
+            ThreeTaskPenaltyTreatmentForm::$TASK_THREE_DEADLINE => '12-03-2015 10:00 am',
+            ThreeTaskPenaltyTreatmentForm::$PAYOFF => '12',
+            ThreeTaskPenaltyTreatmentForm::$TASK_TIME_LIMIT => '60'
         ];
 
-        $form = new SessionForm($post);
+        $form = new \Officium\Framework\View\Forms\ThreeTaskPenaltyTreatmentForm($post);
         $this->assertFalse($form->getSecondaryDeadlineEnabled());
     }
 }
