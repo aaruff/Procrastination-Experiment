190638c124ff24a5f7e5fa6095a72c21a0b972c0
Anwar A. Ruff <anwarruff@gmail.com>
2015-05-23 15:56:12 -0400

    New map

---
+++ app/Officium/Framework/Maps/AttentiveRankMap.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Officium\Framework\Maps;
+
+
+use Officium\Framework\Controllers\AttentiveRankController;
+
+class AttentiveRankMap extends ResourceMap
+{
+    /**
+     * @return string template route
+     */
+    public static function toTemplate()
+    {
+        return '/pages/subject/survey/attentiveRank.twig';
+    }
+
+    /**
+     * @return string
+     */
+    public static function toUri()
+    {
+        return '/survey/att';
+    }
+
+    /**
+     * Returns the path to the controller handling the specified request method.
+     * @param string $method
+     * @return string
+     */
+    public static function toController($method = '')
+    {
+        $controller = get_class(new AttentiveRankController());
+        if ($method == self::$POST) {
+            return $controller . ':post';
+        }
+
+        return $controller . ':get';
+    }
+
+}
