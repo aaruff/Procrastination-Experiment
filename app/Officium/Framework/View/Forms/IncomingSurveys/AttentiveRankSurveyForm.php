c859495bf19eed3d3474dc535f907f59e4fb7a7a
Anwar A. Ruff <anwarruff@gmail.com>
2015-05-23 15:56:52 -0400

    Updated to support save to db

--- app/Officium/Framework/View/Forms/IncomingSurveys/AttentiveRankSurveyForm.php
+++ app/Officium/Framework/View/Forms/IncomingSurveys/AttentiveRankSurveyForm.php
@@ -2,14 +2,12 @@
 
 namespace Officium\Framework\View\Forms\IncomingSurveys;
 
-use Officium\Experiment\IncomingSurveyState;
-use Officium\Framework\Models\Session;
-use Officium\Framework\Models\SessionStorable;
+use Officium\Experiment\AttentiveRankSurvey;
+use Officium\Framework\Models\Saveable;
 use Officium\Framework\Validators\IntegerValidator;
 use Officium\Framework\View\Forms\Form;
-use Officium\Experiment\IncomingSurvey;
 
-class AttentiveRankSurveyForm extends Form implements SessionStorable
+class AttentiveRankSurveyForm extends Form implements Saveable
 {
     private static $CONSCIENTIOUSNESS = 'con';
     private static $FREQUENCY_ASSIGNMENT_LATENESS = 'late';
@@ -25,47 +23,25 @@ class AttentiveRankSurveyForm extends Form implements SessionStorable
      * ------------------------------------------------------------------------------------------ */
     public function __construct($entries = [])
     {
-        parent::__construct(IncomingSurveyState::ATTENTIVE_RANK, $entries, $this->getFormValidators());
+        parent::__construct(get_class($this), $entries, $this->getFormValidators());
     }
 
     /**
      * Stores properties to the session.
-     *
-     * @return void
      */
-    public function saveToSession()
+    public function save()
     {
-        $entries = [
-            self::$CONSCIENTIOUSNESS=>$this->getConcientiousness(),
-            self::$FREQUENCY_ASSIGNMENT_LATENESS=>$this->getFrequencyAssignmentLateness(),
-            self::$TARDINESS=>$this->getTardiness(),
-            self::$EXTERNAL_DISTRACTIONS=>$this->getExternalDistractions(),
-            self::$DEPENDABILITY=>$this->getDependability(),
-            self::$ABILITY_FOLLOW_SCHEDULE=>$this->getAbilityToFollowSchedule(),
-            self::$ABILITY_TO_ORGANIZE=>$this->getAbilityToOrganize(),
-            self::$ABILITY_PAY_ATTENTION=>$this->getAbilityToPayAttention()
-        ];
+        $survey = new AttentiveRankSurvey();
+        $survey->setConscientiousness($this->getIntEntry(self::$CONSCIENTIOUSNESS));
+        $survey->setAssignmentLateness($this->getIntEntry(self::$FREQUENCY_ASSIGNMENT_LATENESS));
+        $survey->setTardiness($this->getIntEntry(self::$TARDINESS));
+        $survey->setExternalDistractions($this->getIntEntry(self::$EXTERNAL_DISTRACTIONS));
+        $survey->setDependability($this->getIntEntry(self::$DEPENDABILITY));
+        $survey->setAbilityFollowSchedule($this->getIntEntry(self::$ABILITY_FOLLOW_SCHEDULE));
+        $survey->setAbilityOrganize($this->getIntEntry(self::$ABILITY_TO_ORGANIZE));
+        $survey->setAbilityPayAttention($this->getIntEntry(self::$ABILITY_PAY_ATTENTION));
 
-        $surveyId = Session::getSurveyId();
-        Session::storeSurveyFormEntries($surveyId, $entries);
-    }
-
-    /**
-     * @param IncomingSurvey $incomingSurvey
-     * @return IncomingSurvey
-     */
-    public function setIncomingSurveyFromEntries(IncomingSurvey $incomingSurvey)
-    {
-        $incomingSurvey->setRankConscientiousness($this->getIntEntry(self::$CONSCIENTIOUSNESS));
-        $incomingSurvey->setRankAssignmentLateness($this->getIntEntry(self::$FREQUENCY_ASSIGNMENT_LATENESS));
-        $incomingSurvey->setRankTardiness($this->getIntEntry(self::$TARDINESS));
-        $incomingSurvey->setRankExternalDistractions($this->getIntEntry(self::$EXTERNAL_DISTRACTIONS));
-        $incomingSurvey->setRankDependability($this->getIntEntry(self::$DEPENDABILITY));
-        $incomingSurvey->setRankAbilityFollowSchedule($this->getIntEntry(self::$ABILITY_FOLLOW_SCHEDULE));
-        $incomingSurvey->setRankAbilityOrganize($this->getIntEntry(self::$ABILITY_TO_ORGANIZE));
-        $incomingSurvey->setRankAbilityPayAttention($this->getIntEntry(self::$ABILITY_PAY_ATTENTION));
-
-        return $incomingSurvey;
+        $survey->save();
     }
 
     /* ------------------------------------------------------------------------------------------
@@ -89,52 +65,5 @@ class AttentiveRankSurveyForm extends Form implements SessionStorable
         $validators[self::$ABILITY_TO_ORGANIZE] = new IntegerValidator(1, 5);
         $validators[self::$ABILITY_PAY_ATTENTION] = new IntegerValidator(1, 5);
         return $validators;
-    }
-
-    /* ------------------------------------------------------------------------------------------
-     *                                      Private
-     * ------------------------------------------------------------------------------------------ */
-
-    /**
-     * @return int
-     */
-    private function getConcientiousness()
-    {
-        return intval($this->getEntries()[self::$CONSCIENTIOUSNESS]);
-    }
-
-    private function getFrequencyAssignmentLateness()
-    {
-        return intval($this->getEntries()[self::$FREQUENCY_ASSIGNMENT_LATENESS]);
-    }
-
-    private function getTardiness()
-    {
-        return intval($this->getEntries()[self::$TARDINESS]);
-    }
-
-    private function getExternalDistractions()
-    {
-        return intval($this->getEntries()[self::$EXTERNAL_DISTRACTIONS]);
-    }
-
-    private function getDependability()
-    {
-        return intval($this->getEntries()[self::$DEPENDABILITY]);
-    }
-
-    private function getAbilityToFollowSchedule()
-    {
-        return intval($this->getEntries()[self::$ABILITY_FOLLOW_SCHEDULE]);
-    }
-
-    private function getAbilityToOrganize()
-    {
-        return intval($this->getEntries()[self::$ABILITY_TO_ORGANIZE]);
-    }
-
-    private function getAbilityToPayAttention()
-    {
-        return intval($this->getEntries()[self::$ABILITY_PAY_ATTENTION]);
     }
 }
