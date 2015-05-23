b2c854d0d48a8c5074dd181c860e5d0436c34f19
Anwar A. Ruff <anwarruff@gmail.com>
2015-05-23 16:02:38 -0400

    Refactored to interface

--- app/Officium/Framework/Maps/StateMap.php
+++ app/Officium/Framework/Maps/StateMap.php
@@ -2,14 +2,11 @@
 
 namespace Officium\Framework\Maps;
 
-class StateMap extends ResourceMap
+interface StateMap
 {
-    private static $routes = [
-        0 => '/survey',
-    ];
-
-    public static function toUri($state)
-    {
-        return self::$routes[$state];
-    }
+    /**
+     * Returns the URI for the current state.
+     * @return string
+     */
+    public function toUri();
 }
