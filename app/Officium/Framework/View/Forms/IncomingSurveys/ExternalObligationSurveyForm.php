d5dde816bf8fd83dedb3f4ffc06241d02bb21076
Anwar A. Ruff <anwarruff@gmail.com>
2015-05-23 15:59:23 -0400

    Updated to support save to db

--- app/Officium/Framework/View/Forms/IncomingSurveys/ExternalObligationSurveyForm.php
+++ app/Officium/Framework/View/Forms/IncomingSurveys/ExternalObligationSurveyForm.php
@@ -2,18 +2,17 @@
 
 namespace Officium\Framework\View\Forms\IncomingSurveys;
 
-use Officium\Experiment\IncomingSurveyState;
-use Officium\Framework\Models\Session;
-use Officium\Framework\Models\SessionStorable;
+use Officium\Experiment\ExternalObligationSurvey as Survey;
+use Officium\Framework\Models\Saveable;
 use Officium\Framework\Validators\ArrayValidator;
 use Officium\Framework\Validators\IntegerValidator;
 use Officium\Framework\Validators\DateTimeValidator;
 use Officium\Framework\Validators\SelectValidator;
 use Officium\Framework\View\Forms\Form;
-use Officium\Experiment\IncomingSurvey;
-use Officium\Experiment\SurveyDateTime;
+use Officium\Framework\Models\User;
+use Officium\Experiment\ExternalObligationDeadline as Deadline;
 
-class ExternalObligationSurveyForm extends Form implements SessionStorable
+class ExternalObligationSurveyForm extends Form implements Saveable
 {
     private static $DATE_TIME_FORMAT = 'm-d-Y g:i a';
 
@@ -28,49 +27,25 @@ class ExternalObligationSurveyForm extends Form implements SessionStorable
 
     public function __construct($entries = [])
     {
-        parent::__construct(IncomingSurveyState::EXTERNAL_OBLIGATION, $entries, $this->getFormValidators());
+        parent::__construct(get_class($this), $entries, $this->getFormValidators());
     }
 
     /**
      * Stores properties to the session.
      *
-     * @return void
+     * @param User $user
      */
-    public function saveToSession()
+    public function save(User $user)
     {
-        $entries = [
-            self::$EMPLOYED=>$this->getIntEntry(self::$EMPLOYED),
-            self::$HOURS_WORK=>$this->getIntEntry(self::$HOURS_WORK),
-            self::$HOURS_SOCIAL=>$this->getIntEntry(self::$HOURS_SOCIAL),
-            self::$HOURS_FAMILY=>$this->getIntEntry(self::$HOURS_FAMILY),
+        $survey = new Survey();
+        $survey->setEmployed($this->getIntEntry(self::$EMPLOYED));
+        $survey->setHoursWork($this->getIntEntry(self::$HOURS_WORK));
+        $survey->setHoursSocialObligations($this->getIntEntry(self::$HOURS_SOCIAL));
+        $survey->setHoursFamilyObligations($this->getIntEntry(self::$HOURS_FAMILY));
 
-            SurveyDateTime::$WORK_START_DATE_TIME=>$this->getDateTimes(SurveyDateTime::$WORK_START_DATE_TIME),
-            SurveyDateTime::$WORK_END_DATE_TIME=>$this->getDateTimes(SurveyDateTime::$WORK_END_DATE_TIME),
-
-            SurveyDateTime::$SOCIAL_START_DATE_TIME=>$this->getDateTimes(SurveyDateTime::$SOCIAL_START_DATE_TIME),
-            SurveyDateTime::$SOCIAL_END_DATE_TIME=>$this->getDateTimes(SurveyDateTime::$SOCIAL_END_DATE_TIME),
-
-            SurveyDateTime::$FAMILY_START_DATE_TIME=>$this->getDateTimes(SurveyDateTime::$FAMILY_START_DATE_TIME),
-            SurveyDateTime::$FAMILY_END_DATE_TIME=>$this->getDateTimes(SurveyDateTime::$FAMILY_END_DATE_TIME),
-        ];
-
-        $surveyId = Session::getSurveyId();
-        Session::storeSurveyFormEntries($surveyId, $entries);
+        $survey->externalObligationDeadlines()->saveMany($this->getAllDeadlines());
     }
 
-
-    public function setIncomingSurveyFromEntries(IncomingSurvey $incomingSurvey)
-    {
-        $incomingSurvey->setEmployed($this->getIntEntry(self::$EMPLOYED));
-        $incomingSurvey->setHoursWork($this->getIntEntry(self::$HOURS_WORK));
-        $incomingSurvey->setHoursSocialObligations($this->getIntEntry(self::$HOURS_SOCIAL));
-        $incomingSurvey->setHoursFamilyObligations($this->getIntEntry(self::$HOURS_FAMILY));
-        $incomingSurvey->setSurveyDateTimes($this->getSurveyDateTime(SurveyDateTime::$WORK_START_DATE_TIME, SurveyDateTime::$WORK_END_DATE_TIME));
-        $incomingSurvey->setSurveyDateTimes($this->getSurveyDateTime(SurveyDateTime::$SOCIAL_START_DATE_TIME, SurveyDateTime::$SOCIAL_END_DATE_TIME));
-        $incomingSurvey->setSurveyDateTimes($this->getSurveyDateTime(SurveyDateTime::$FAMILY_START_DATE_TIME, SurveyDateTime::$FAMILY_END_DATE_TIME));
-
-        return $incomingSurvey;
-    }
 
     /* ------------------------------------------------------------------------------------------
      *                                      Protected
@@ -88,12 +63,13 @@ class ExternalObligationSurveyForm extends Form implements SessionStorable
         $validators[self::$HOURS_WORK] = new IntegerValidator(0, 200);
         $validators[self::$HOURS_SOCIAL] = new IntegerValidator(0, 200);
         $validators[self::$HOURS_FAMILY] = new IntegerValidator(0, 200);
-        $validators[SurveyDateTime::$WORK_START_DATE_TIME] = new ArrayValidator(new DateTimeValidator(self::$DATE_TIME_FORMAT, false));
-        $validators[SurveyDateTime::$WORK_END_DATE_TIME] = new ArrayValidator(new DateTimeValidator(self::$DATE_TIME_FORMAT, false));
-        $validators[SurveyDateTime::$SOCIAL_START_DATE_TIME] = new ArrayValidator(new DateTimeValidator(self::$DATE_TIME_FORMAT, false));
-        $validators[SurveyDateTime::$SOCIAL_END_DATE_TIME] = new ArrayValidator(new DateTimeValidator(self::$DATE_TIME_FORMAT, false));
-        $validators[SurveyDateTime::$FAMILY_START_DATE_TIME] = new ArrayValidator(new DateTimeValidator(self::$DATE_TIME_FORMAT, false));
-        $validators[SurveyDateTime::$FAMILY_END_DATE_TIME] = new ArrayValidator(new DateTimeValidator(self::$DATE_TIME_FORMAT, false));
+        $validators[Deadline::$WORK_START_DATE_TIME] = new ArrayValidator(new DateTimeValidator(self::$DATE_TIME_FORMAT, false));
+        $validators[Deadline::$WORK_END_DATE_TIME] = new ArrayValidator(new DateTimeValidator(self::$DATE_TIME_FORMAT, false));
+        $validators[Deadline::$SOCIAL_START_DATE_TIME] = new ArrayValidator(new DateTimeValidator(self::$DATE_TIME_FORMAT, false));
+        $validators[Deadline::$SOCIAL_END_DATE_TIME] = new ArrayValidator(new DateTimeValidator(self::$DATE_TIME_FORMAT, false));
+        $validators[Deadline::$FAMILY_START_DATE_TIME] = new ArrayValidator(new DateTimeValidator(self::$DATE_TIME_FORMAT, false));
+        $validators[Deadline::$FAMILY_END_DATE_TIME] = new ArrayValidator(new DateTimeValidator(self::$DATE_TIME_FORMAT, false));
+        $validators[self::$SEMANTIC_VALIDATORS] = [new UserValidator(new User())];
         //TODO: Add range validation
         return $validators;
     }
@@ -102,35 +78,29 @@ class ExternalObligationSurveyForm extends Form implements SessionStorable
      *                                      Private
      * ------------------------------------------------------------------------------------------ */
 
-    private function getSurveyDateTime($typeStart, $typeEnd)
+    private function getAllDeadlines()
     {
         $entries = $this->getEntries();
 
-        $dateTimes = [];
-        foreach ($entries[$typeStart] as $startDateTime) {
-            $surveyDateTime = new SurveyDateTime();
-            $surveyDateTime->setStartDateTime(\DateTime::createFromFormat(self::$DATE_TIME_FORMAT, $startDateTime));
-            $surveyDateTime->setType($typeStart);
-            $dateTimes[] = $surveyDateTime;
-        }
+        $deadlines = [
+            ['start'=>Deadline::$SOCIAL_START_DATE_TIME, 'end'=>Deadline::$SOCIAL_END_DATE_TIME],
+            ['start'=>Deadline::$WORK_START_DATE_TIME, 'end'=>Deadline::$WORK_END_DATE_TIME],
+            ['start'=>Deadline::$FAMILY_START_DATE_TIME, 'end'=>Deadline::$FAMILY_END_DATE_TIME]];
 
-        /* @var \Officium\Experiment\SurveyDateTime[] $dateTimes */
-        foreach ($dateTimes as $id=>$dateTime) {
-            if (isset($entries[$typeEnd][$id])) {
-                $dateTime->setEndDateTime(\DateTime::createFromFormat(self::$DATE_TIME_FORMAT, $entries[$typeEnd][$id]));
+        $dateTimes = [];
+        foreach ($deadlines as $deadline) {
+            $dateTimes = [];
+            foreach ($entries[$deadline['start']] as $id=>$start) {
+                $surveyDateTime = new Deadline();
+                $surveyDateTime->setStartDateTime(\DateTime::createFromFormat(self::$DATE_TIME_FORMAT, $start));
+                $surveyDateTime->setType($start);
+                $dateTimes[] = $surveyDateTime;
+
+                if (isset($entries[$deadline['end']][$id])) {
+                    $surveyDateTime->setEndDateTime(\DateTime::createFromFormat(self::$DATE_TIME_FORMAT, $entries[$deadline['end']][$id]));
+                }
+
             }
-        }
-
-        return $dateTimes;
-    }
-
-    private function getDateTimes($type)
-    {
-        $entries = $this->getEntries();
-
-        $dateTimes = [];
-        foreach ($entries[$type] as $dateTime) {
-            $dateTimes[] = \DateTime::createFromFormat(self::$DATE_TIME_FORMAT, $dateTime);
         }
 
         return $dateTimes;
