90ffbd6083627e5df9cce251cc9e076c0e173817
Anwar A. Ruff <anwarruff@gmail.com>
2015-05-23 16:02:45 -0400

    New map

---
+++ app/Officium/Experiment/StateMapFactory.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Officium\Experiment;
+
+
+use Officium\Framework\Maps\ThreeTaskPenaltyStateMap;
+
+class StateMapFactory
+{
+    public static function getStateMap(Subject $subject)
+    {
+        /* @var \Officium\Experiment\Treatment $treatment */
+        $treatment = $subject->session->treatment;
+
+        if ($treatment->getType() == Treatment::$THREE_TASK_TIME_LIMIT_PENALTY_ADJUSTABLE_DEADLINE) {
+            return new ThreeTaskPenaltyStateMap($subject);
+        }
+    }
+
+}
