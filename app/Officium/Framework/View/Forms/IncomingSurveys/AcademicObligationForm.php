67173744731da1805c73b0a1e4db90c662567ec6
Anwar A. Ruff <anwarruff@gmail.com>
2015-05-23 15:55:19 -0400

    Refactored to support direct save to db

--- app/Officium/Framework/View/Forms/IncomingSurveys/AcademicObligationForm.php
+++ app/Officium/Framework/View/Forms/IncomingSurveys/AcademicObligationForm.php
@@ -2,17 +2,16 @@
 
 namespace Officium\Framework\View\Forms\IncomingSurveys;
 
-use Officium\Experiment\IncomingSurvey;
-use Officium\Experiment\IncomingSurveyState;
-use Officium\Experiment\SurveyDateTime;
-use Officium\Framework\Models\Session;
+use Officium\Experiment\AcademicObligationSurvey;
+use Officium\Experiment\AcademicObligationDeadline as Deadline;
 use Officium\Framework\Validators\ArrayValidator;
 use Officium\Framework\Validators\DateTimeValidator;
 use Officium\Framework\Validators\IntegerValidator;
-use Officium\Framework\Models\SessionStorable;
+use Officium\Framework\Models\Saveable;
 use Officium\Framework\View\Forms\Form;
+use Officium\Framework\Models\User;
 
-class AcademicObligationForm extends Form implements SessionStorable
+class AcademicObligationForm extends Form implements Saveable
 {
     private static $DATE_TIME_FORMAT = 'm-d-Y g:i a';
 
@@ -28,48 +27,27 @@ class AcademicObligationForm extends Form implements SessionStorable
 
     public function __construct($entries = [])
     {
-        parent::__construct(IncomingSurveyState::ACADEMIC_OBLIGATION, $entries, $this->getFormValidators());
+        parent::__construct(get_class($this), $entries, $this->getFormValidators());
     }
 
     /**
      * Stores properties to the session.
      *
+     * @param User $user
      * @return void
      */
-    public function saveToSession()
+    public function save(User $user)
     {
-        $entries = [
-            self::$HOURS_COURSE_WORK=>$this->getHoursCourseWork(),
-            self::$NUM_MAJOR=>$this->getNumMajorAssignments(),
-            self::$NUM_MINOR=>$this->getNumMinorAssignments(),
-            self::$NUM_EXAM=>$this->getNumExams(),
-            SurveyDateTime::$MINOR_DEADLINE_ID=>$this->getDateTimes(SurveyDateTime::$MINOR_DEADLINE_ID),
-            SurveyDateTime::$MAJOR_DEADLINE_ID=>$this->getDateTimes(SurveyDateTime::$MAJOR_DEADLINE_ID),
-            SurveyDateTime::$EXAM_DEADLINE_ID=>$this->getDateTimes(SurveyDateTime::$EXAM_DEADLINE_ID),
-        ];
+        $survey = new AcademicObligationSurvey();
+        $survey->setHoursCourseWork($this->getIntEntry(self::$HOURS_COURSE_WORK));
+        $survey->setNumMinorAssignments($this->getIntEntry(self::$NUM_MINOR));
+        $survey->setNumMajorAssignments($this->getIntEntry(self::$NUM_MAJOR));
+        $survey->setNumberExams($this->getIntEntry(self::$NUM_EXAM));
+        $survey->setSubjectId($user->getSubject()->getId());
+        $survey->save();
 
-        $surveyId = Session::getSurveyId();
-        Session::storeSurveyFormEntries($surveyId, $entries);
+        $survey->academicObligationDeadlines()->saveMany($this->getAllDeadlines());
     }
-
-    /**
-     * @param IncomingSurvey $incomingSurvey
-     * @return IncomingSurvey
-     */
-    public function setIncomingSurveyFromEntries(IncomingSurvey $incomingSurvey)
-    {
-        $incomingSurvey->setHoursCourseWork($this->getHoursCourseWork());
-        $incomingSurvey->setNumMinorAssignments($this->getNumMinorAssignments());
-        $incomingSurvey->setNumMajorAssignments($this->getNumMajorAssignments());
-        $incomingSurvey->setNumberExams($this->getNumExams());
-        $incomingSurvey->setSurveyDateTimes($this->getSurveyDateTime(SurveyDateTime::$MINOR_DEADLINE_ID));
-        $incomingSurvey->setSurveyDateTimes($this->getSurveyDateTime(SurveyDateTime::$MAJOR_DEADLINE_ID));
-        $incomingSurvey->setSurveyDateTimes($this->getSurveyDateTime(SurveyDateTime::$EXAM_DEADLINE_ID));
-
-        return $incomingSurvey;
-    }
-
-
 
     /* ------------------------------------------------------------------------------------------
      *                                      Protected
@@ -87,9 +65,9 @@ class AcademicObligationForm extends Form implements SessionStorable
         $validators[self::$NUM_MINOR] = new IntegerValidator(0, 40);
         $validators[self::$NUM_MAJOR] = new IntegerValidator(0, 40);
         $validators[self::$NUM_EXAM] = new IntegerValidator(0, 40);
-        $validators[SurveyDateTime::$MINOR_DEADLINE_ID] = new ArrayValidator(new DateTimeValidator(self::$DATE_TIME_FORMAT, false));
-        $validators[SurveyDateTime::$MAJOR_DEADLINE_ID] = new ArrayValidator(new DateTimeValidator(self::$DATE_TIME_FORMAT, false));
-        $validators[SurveyDateTime::$EXAM_DEADLINE_ID] = new ArrayValidator(new DateTimeValidator(self::$DATE_TIME_FORMAT, false));
+        $validators[Deadline::$MINOR_DEADLINE_ID] = new ArrayValidator(new DateTimeValidator(self::$DATE_TIME_FORMAT, false));
+        $validators[Deadline::$MAJOR_DEADLINE_ID] = new ArrayValidator(new DateTimeValidator(self::$DATE_TIME_FORMAT, false));
+        $validators[Deadline::$EXAM_DEADLINE_ID] = new ArrayValidator(new DateTimeValidator(self::$DATE_TIME_FORMAT, false));
 
         return $validators;
     }
@@ -98,70 +76,25 @@ class AcademicObligationForm extends Form implements SessionStorable
      *                                      Private
      * ------------------------------------------------------------------------------------------ */
 
-    private function getSurveyDateTime($type)
+    /**
+     * @return \Officium\Experiment\AcademicObligationDeadline[]
+     */
+    private function getAllDeadlines()
     {
         $entries = $this->getEntries();
 
+        $deadlineTypes = [Deadline::$MAJOR_DEADLINE_ID, Deadline::$MINOR_DEADLINE_ID, Deadline::$EXAM_DEADLINE_ID];
+
         $dateTimes = [];
-        foreach ($entries[$type] as $dateTime) {
-            $surveyDateTime = new SurveyDateTime();
-            $surveyDateTime->setStartDateTime(\DateTime::createFromFormat(self::$DATE_TIME_FORMAT, $dateTime));
-            $surveyDateTime->setType($type);
-            $dateTimes[] = $surveyDateTime;
+        foreach ($deadlineTypes as $type) {
+            foreach ($entries[$type] as $dateTime) {
+                $surveyDateTime = new Deadline();
+                $surveyDateTime->setDeadline(\DateTime::createFromFormat(self::$DATE_TIME_FORMAT, $dateTime));
+                $surveyDateTime->setType($type);
+                $dateTimes[] = $surveyDateTime;
+            }
         }
 
         return $dateTimes;
-    }
-
-    /**
-     * @param $type
-     * @return array
-     */
-    private function getDateTimes($type)
-    {
-        $entries = $this->getEntries();
-
-        $dateTimes = [];
-        foreach ($entries[$type] as $dateTime) {
-            $dateTimes[] = \DateTime::createFromFormat(self::$DATE_TIME_FORMAT, $dateTime);
-        }
-
-        return $dateTimes;
-    }
-
-    /**
-     * @return int
-     */
-    private function getHoursCourseWork()
-    {
-        $entries = $this->getEntries();
-        return intval($entries[self::$HOURS_COURSE_WORK]);
-    }
-
-    /**
-     * @return int
-     */
-    private function getNumMinorAssignments()
-    {
-        $entries = $this->getEntries();
-        return intval($entries[self::$NUM_MINOR]);
-    }
-
-    /**
-     * @return int
-     */
-    private function getNumMajorAssignments()
-    {
-        $entries = $this->getEntries();
-        return intval($entries[self::$NUM_MAJOR]);
-    }
-
-    /**
-     * @return int
-     */
-    private function getNumExams()
-    {
-        $entries = $this->getEntries();
-        return intval($entries[self::$NUM_EXAM]);
     }
 }
