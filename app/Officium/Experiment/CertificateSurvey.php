ca058c751aba3287ba2ab446d06c7b44e92c79cc
Anwar A. Ruff <anwarruff@gmail.com>
2015-05-23 15:57:37 -0400

    New model

---
+++ app/Officium/Experiment/CertificateSurvey.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Officium\Experiment;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+class CertificateSurvey extends Model
+{
+    protected $table = 'certificate_surveys';
+
+    /* ------------------------------------------------------------------------------------------
+     *                                Eloquent Relations
+     * ------------------------------------------------------------------------------------------ */
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function subject()
+    {
+        return $this->belongsTo(get_class(new Subject()), 'subject_id');
+    }
+
+
+    /* ------------------------------------------------------------------------------------------
+     *                                      Public
+     * ------------------------------------------------------------------------------------------ */
+
+    /**
+     * @param int $certificates
+     */
+    public function setCertificatesYear($certificates)
+    {
+        $this->certificates_year = $certificates;
+    }
+
+    /**
+     * @param int $temptation
+     */
+    public function setTemptation($temptation)
+    {
+        $this->temptation = $temptation;
+    }
+
+    /**
+     * @param int $certificates
+     */
+    public function setTemptationCertificatesYear($certificates)
+    {
+        $this->temptation_certificates_year = $certificates;
+    }
+
+    /**
+     * @param int $nights
+     */
+    public function setNightsPerYear($nights)
+    {
+        $this->nights_per_year = $nights;
+    }
+
+}
