261cf9fd0790eda1902a2a2469981d3734bbf365
Anwar A. Ruff <anwarruff@gmail.com>
2015-05-24 20:59:02 -0400

    Added entry types and getter function

--- app/Officium/Framework/Validators/Validator.php
+++ app/Officium/Framework/Validators/Validator.php
@@ -10,7 +10,8 @@ namespace Officium\Framework\Validators;
 abstract class Validator
 {
     protected static $NOT_EMPTY = 'This field is required.';
-    protected static $TRUE = 'This field must be either selected or unselected.';
+    protected static $TRUE = 'This field must be true.';
+    protected static $FALSE = 'This field must be false';
     protected static $DATE_TIME = 'This field requires a validly formatted Date and Time.';
     protected static $STRING = 'This field must consist of only text.';
     protected static $FLOAT = 'This field must be a floating point number.';
@@ -24,12 +25,33 @@ abstract class Validator
      */
     protected $errors = [];
 
+    public static $SINGLE_ENTRY = 1;
+    Public static $ALL_ENTRIES = 2;
+
+    /* ------------------------------------------------------------------------------------------
+     *                                      Abstract
+     * ------------------------------------------------------------------------------------------ */
+
     /**
      * Validates the entry provided.
      * @param $entry
      * @return boolean
      */
     abstract public function validate($entry);
+
+
+    /* ------------------------------------------------------------------------------------------
+     *                                       Public
+     * ------------------------------------------------------------------------------------------ */
+    /**
+     * Returns the entry type.
+     *
+     * @return int Entry type
+     */
+    public function getEntryType() {
+        return self::$SINGLE_ENTRY;
+    }
+
 
     /**
      * Sets the error message.
