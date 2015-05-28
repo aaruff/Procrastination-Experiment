41257ac0c8691ad855fb048175069db4cb7d500f
Anwar A. Ruff <anwarruff@gmail.com>
2015-05-28 15:23:09 -0400

    New Controller

---
+++ app/Officium/Framework/Controllers/RankTaskCompletionController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Officium\Framework\Controllers;
+
+use Slim\Slim;
+use Officium\Framework\Maps\RankTaskCompletionMap as Map;
+use Officium\Framework\Models\Session;
+use Officium\Framework\View\Forms\IncomingSurveys\RankTaskCompletionForm as Form;
+use Officium\Framework\Maps\SurveyCompleteMap;
+
+
+
+class RankTaskCompletionController
+{
+    /**
+     * Handles get requests
+     */
+    public function get()
+    {
+        $app = Slim::getInstance();
+        $app->render(Map::toTemplate(), $app->flashData());
+    }
+
+    /**
+     * Handles post requests
+     */
+    public function post()
+    {
+        $app = Slim::getInstance();
+
+        $form = new Form();
+        if ( ! $form->validate($app->request->post())) {
+            $app->flash('flash', $form->getEntriesWithErrors());
+            $app->redirect(Map::toUri());
+            return;
+        }
+
+        $form->save(Session::getUser());
+
+        $subject = Session::getSubject();
+        $subject->setNextState();
+        $subject->save();
+
+        $app->render(SurveyCompleteMap::toTemplate());
+    }
+}
