bd3612936f04872ba5e3f1ac1a6ebb6fd3c8004e
Anwar A. Ruff <anwarruff@gmail.com>
2015-05-27 21:12:50 -0400

    Renamed several vars

---
+++ app/Officium/Framework/Validators/RequiredConditionalValidator.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Officium\Framework\Validators;
+
+
+class RequiredConditionalValidator extends Validator
+{
+    private $predicates;
+    private $consequentValidator;
+    private $consequentKey;
+
+    /**
+     * @param array $predicates The (keys, validator) pairs that must be true before the $validator is applied.
+     * @param string $consequentKey The key used to identify the entry for validation.
+     * @param Validator $consequentValidator The validator applied to the entry provided.
+     */
+    public function __construct(array $predicates, $consequentKey, Validator $consequentValidator)
+    {
+        $this->predicates = $predicates;
+        $this->consequentValidator = $consequentValidator;
+        $this->consequentKey = $consequentKey;
+    }
+
+
+    /**
+     * Validates the entry provided if the predicates are true.
+     *
+     * @param $entries
+     * @return boolean
+     */
+    public function validate($entries)
+    {
+        $this->clearErrors();
+        // This validator requires array entries
+        if ( ! is_array($entries)) {
+            return false;
+        }
+
+        $validationPassed  = true;
+        foreach ($this->predicates as $keyValPair) {
+            $predicateKey = $keyValPair[0];
+            /* @var \Officium\Framework\Validators\Validator $predicateValidator */
+            $predicateValidator = $keyValPair[1];
+
+            $entry = ($predicateValidator->getEntryType() == Validator::$SINGLE_ENTRY) ? $entries[$predicateKey] : $entries;
+            if ( ! $predicateValidator->validate($entry)) {
+                $validationPassed = false;
+            }
+        }
+
+        if ( ! $validationPassed) {
+            return false;
+        }
+
+        if ( ! $this->consequentValidator->validate($entries[$this->consequentKey])) {
+            $this->setErrors($this->consequentValidator->getErrors());
+            return false;
+        }
+
+        return true;
+    }
+
+    public function getEntryType()
+    {
+        return Validator::$ALL_ENTRIES;
+    }
+
+}
