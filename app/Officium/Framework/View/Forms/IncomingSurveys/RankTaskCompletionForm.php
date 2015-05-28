3da2b08a432728970cd1463eef104dabe4a17f9c
Anwar A. Ruff <anwarruff@gmail.com>
2015-05-28 15:23:28 -0400

    New form class

---
+++ app/Officium/Framework/View/Forms/IncomingSurveys/RankTaskCompletionForm.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Officium\Framework\View\Forms\IncomingSurveys;
+
+use Officium\Experiment\RankTaskCompletionSurvey;
+use Officium\Framework\Validators\IntegerValidator;
+use Officium\Framework\Models\Saveable;
+use Officium\Framework\Models\User;
+use Officium\Framework\View\Forms\Form;
+
+class RankTaskCompletionForm extends Form implements Saveable
+{
+    private static $NO_TASK = 'n';
+    private static $ONE_TASK = 'o';
+    private static $TWO_TASKS = 't';
+    private static $ALL_TASKS = 'a';
+
+    /* ------------------------------------------------------------------------------------------
+     *                                      Public
+     * ------------------------------------------------------------------------------------------ */
+
+    public function __construct($entries = [])
+    {
+        parent::__construct(get_class($this), $entries, $this->getFormValidators());
+    }
+
+    /**
+     * Stores properties to the session.
+     *
+     * @param User $user
+     *
+     * @return void
+     */
+    public function save(User $user)
+    {
+        $survey = new RankTaskCompletionSurvey();
+        $survey->setNoTaskRank($this->getIntEntry(self::$NO_TASK));
+        $survey->setOneTaskRank($this->getIntEntry(self::$ONE_TASK));
+        $survey->setTwoTaskRank($this->getIntEntry(self::$TWO_TASKS));
+        $survey->setAllTaskRank($this->getIntEntry(self::$ALL_TASKS));
+        $survey->setSubjectId($user->getSubject()->getId());
+        $survey->save();
+    }
+
+    /* ------------------------------------------------------------------------------------------
+     *                                      Protected
+     * ------------------------------------------------------------------------------------------ */
+
+    /**
+     * Returns the form's validators
+     *
+     * @return \Officium\Framework\Validators\Validator[]
+     */
+    protected function getFormValidators()
+    {
+        $validators = [];
+        $validators[self::$NO_TASK] = new IntegerValidator(0, 100);
+        $validators[self::$ONE_TASK] = new IntegerValidator(0, 100);
+        $validators[self::$TWO_TASKS] = new IntegerValidator(0, 100);
+        $validators[self::$ALL_TASKS] = new IntegerValidator(0, 100);
+        return $validators;
+    }
+}
