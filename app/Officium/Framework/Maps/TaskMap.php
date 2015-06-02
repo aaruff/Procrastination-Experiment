654bfc0b4bde9bfbe3acc55a97ecbca7883d2be6
Anwar A. Ruff <anwarruff@gmail.com>
2015-06-01 21:22:34 -0400

    New Map

---
+++ app/Officium/Framework/Maps/TaskMap.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Officium\Framework\Maps;
+
+
+use Officium\Framework\Controllers\TaskController;
+
+class TaskMap extends ResourceMap
+{
+    /**
+     * @return string template route
+     */
+    public static function toTemplate()
+    {
+        return '/pages/subject/survey/task.twig';
+    }
+
+    /**
+     * @return string
+     */
+    public static function toUri()
+    {
+        return '/survey/task';
+    }
+
+    /**
+     * Returns the path to the controller handling the specified request method.
+     * @param string $method
+     * @return string
+     */
+    public static function toController($method = '')
+    {
+        $controller = get_class(new TaskController());
+        if ($method == self::$POST) {
+            return $controller . ':post';
+        }
+
+        return $controller . ':get';
+    }
+}
