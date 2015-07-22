e079262cd0260f1b70985793915117240672b4aa
Anwar A. Ruff <anwarruff@gmail.com>
2015-07-22 13:06:09 -0400

    Updated get function to reflect use new SubjectGame class

--- app/Officium/Framework/Controllers/LandingPageController.php
+++ app/Officium/Framework/Controllers/LandingPageController.php
@@ -2,10 +2,13 @@
 
 namespace Officium\Framework\Controllers;
 
+use Officium\Experiment\SubjectGame;
 use Officium\Framework\View\Tables\LandingPageTable;
 use Slim\Slim;
 use Officium\Framework\Models\Session;
 use Officium\Framework\Maps\LandingPageMap as Map;
+use Officium\Framework\Maps\OutgoingQuestionnaireMap as OutgoingMap;
+use Officium\Experiment\EventLog;
 
 
 class LandingPageController 
@@ -16,10 +19,21 @@ class LandingPageController
     public function get()
     {
         $app = Slim::getInstance();
-        $table = new LandingPageTable();
+        $subject = Session::getSubject();
+        EventLog::logEvent($subject, EventLog::LANDING_PAGE_REQUEST);
 
-        $app->flash('table', $table->getData(Session::getSubject()));
-        $app->render(Map::toTemplate(), $app->flashData());
+        $game = new SubjectGame($subject);
+        if ($game->isOver()) {
+            $subject = Session::getSubject();
+            $subject->setNextState();
+            $subject->save();
+            $app->redirect(OutgoingMap::toUri());
+            return;
+        }
+
+        $table = new LandingPageTable();
+        $tableData = $table->getData(Session::getSubject());
+        $app->render(Map::toTemplate(), $tableData);
     }
 
 }
