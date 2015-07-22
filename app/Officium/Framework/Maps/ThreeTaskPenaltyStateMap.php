f10622ca4de388f8271841c8057cc3aad9db11eb
Anwar A. Ruff <anwarruff@gmail.com>
2015-07-22 13:10:51 -0400

    Added getStateUri() func

--- app/Officium/Framework/Maps/ThreeTaskPenaltyStateMap.php
+++ app/Officium/Framework/Maps/ThreeTaskPenaltyStateMap.php
@@ -4,6 +4,8 @@ namespace Officium\Framework\Maps;
 
 use Officium\Experiment\Subject;
 use Officium\Experiment\ThreeTaskPenaltyGameState;
+use Slim\Slim;
+use Officium\Experiment\SubjectGame;
 
 class ThreeTaskPenaltyStateMap implements StateMap
 {
@@ -11,33 +13,73 @@ class ThreeTaskPenaltyStateMap implements StateMap
      * @var \Officium\Experiment\Subject
      */
     private $subject;
+    private $tasks;
 
     public function __construct(Subject $subject)
     {
         $this->subject = $subject;
     }
 
-    public function toUriList()
+    public function isStateValidUri($uri)
     {
         switch($this->subject->getState()) {
             case ThreeTaskPenaltyGameState::GENERAL:
-                return [GeneralAcademicMap::toUri()];
+                return GeneralAcademicMap::isUri($uri);
             case ThreeTaskPenaltyGameState::ACADEMIC_OBLIGATION:
-                return [AcademicObligationMap::toUri()];
+                return AcademicObligationMap::isUri($uri);
             case ThreeTaskPenaltyGameState::EXTERNAL_OBLIGATION:
-                return [ExternalObligationMap::toUri()];
+                return ExternalObligationMap::isUri($uri);
             case ThreeTaskPenaltyGameState::ATTENTIVE_RANK:
-                return [AttentiveRankMap::toUri()];
+                return AttentiveRankMap::isUri($uri);
             case ThreeTaskPenaltyGameState::CERTIFICATE:
-                return [CertificateMap::toUri()];
+                return CertificateMap::isUri($uri);
             case ThreeTaskPenaltyGameState::DEADLINE:
-                return [DeadlineMap::toUri()];
+                return DeadlineMap::isUri($uri);
             case ThreeTaskPenaltyGameState::RANK_TASK_COMPLETION:
-                return [RankTaskCompletionMap::toUri()];
+                return RankTaskCompletionMap::isUri($uri);
             case ThreeTaskPenaltyGameState::TASK:
-                return [LandingPageMap::toUri(), TaskMap::toUri()];
+                if (LandingPageMap::isUri($uri)) {
+                     return true;
+                }
+                elseif(TaskMap::isUri($uri)) {
+                    $taskNumber = TaskMap::getTaskNumber($uri);
+                    $game = new SubjectGame($this->subject);
+                    return $game->isTaskAccessible($taskNumber);
+                 }
+                else {
+                    return false;
+                }
+            case ThreeTaskPenaltyGameState::OUTGOING_SURVEY:
+                return OutgoingQuestionnaireMap::isUri($uri);
             default:
-                return [LoginMap::toUri()];
+                return false;
+        }
+    }
+
+    /**
+     * @return string
+     */
+    public function getStateUri()
+    {
+        switch($this->subject->getState()) {
+            case ThreeTaskPenaltyGameState::GENERAL:
+                return GeneralAcademicMap::toUri();
+            case ThreeTaskPenaltyGameState::ACADEMIC_OBLIGATION:
+                return AcademicObligationMap::toUri();
+            case ThreeTaskPenaltyGameState::EXTERNAL_OBLIGATION:
+                return ExternalObligationMap::toUri();
+            case ThreeTaskPenaltyGameState::ATTENTIVE_RANK:
+                return AttentiveRankMap::toUri();
+            case ThreeTaskPenaltyGameState::CERTIFICATE:
+                return CertificateMap::toUri();
+            case ThreeTaskPenaltyGameState::DEADLINE:
+                return DeadlineMap::toUri();
+            case ThreeTaskPenaltyGameState::RANK_TASK_COMPLETION:
+                return RankTaskCompletionMap::toUri();
+            case ThreeTaskPenaltyGameState::TASK:
+                return LandingPageMap::toUri();
+            default:
+                return LoginMap::toUri();
         }
     }
 }
