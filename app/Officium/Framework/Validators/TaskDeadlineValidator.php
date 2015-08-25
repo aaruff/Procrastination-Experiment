98a1b1c3370ccc91f9bbc7d3ab4062c37a90a97d
Anwar A. Ruff <anwarruff@gmail.com>
2015-08-25 16:14:06 -0400

    Created task deadline validator

---
+++ app/Officium/Framework/Validators/TaskDeadlineValidator.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Officium\Framework\Validators;
+
+
+use Officium\Experiment\Subject;
+
+class TaskDeadlineValidator extends Validator
+{
+    protected static $DATE_TIME_FORMAT = 'm-d-Y h:i a';
+
+    /**
+     * @var \Officium\Framework\Models\Subject
+     */
+    private $subject;
+
+    private $key;
+    private $taskId;
+
+    public function __construct(Subject $subject, $key, $taskId)
+    {
+        $this->subject = $subject;
+        $this->key = $key;
+        $this->taskId = $taskId;
+    }
+
+    /**
+     * Validates the entry provided.
+     * @param $entries
+     *
+     * @return boolean
+     */
+    public function validate($entries)
+    {
+        $this->clearErrors();
+
+        $tasks = $this->subject->getSession()->getTreatment()->getTasks();
+        $task = $tasks[$this->taskId - 1];
+            $subjectDeadline = \DateTime::createFromFormat(self::$DATE_TIME_FORMAT, $entries[$this->key]);
+            if ($task->getDeadline() < $subjectDeadline) {
+                $this->setErrors([$this->key => 'Your deadline must be before or on ' . $task->getDeadline()->format('m/d/Y g:i a') . '.' ]);
+                return false;
+            }
+
+        return true;
+    }
+
+
+    public function getEntryType()
+    {
+        return Validator::$ALL_ENTRIES;
+    }
+}
+
