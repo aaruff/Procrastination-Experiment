2b06b4aeef086bb38ff7d5d67b2c3957c5aabb73
Anwar A. Ruff <anwarruff@gmail.com>
2015-09-10 12:49:52 -0400

    Renamed class

--- app/Officium/Framework/View/Forms/OutgoingSurveyForm.php
+++ app/Officium/Framework/View/Forms/OutgoingSurveyForm.php
@@ -2,14 +2,17 @@
 
 namespace Officium\Framework\View\Forms;
 
+use Officium\Experiment\OutgoingSurvey;
 use Officium\Experiment\Subject;
 use Officium\Framework\Models\Saveable;
+use Officium\Framework\Models\Session;
 use Officium\Framework\Models\User;
 use Officium\Framework\Validators\IntegerValidator;
 use Officium\Framework\Validators\SelectionValidator;
+use Officium\Framework\Validators\YesNoValidator;
 use Slim\Slim;
 
-class OutgoingQuestionnaireForm extends Form implements Saveable
+class OutgoingSurveyForm extends Form implements Saveable
 {
     protected static $DISPLAY_DATE_TIME_FORMAT = 'n/j/y g:i a';
 
@@ -39,11 +42,11 @@ class OutgoingQuestionnaireForm extends Form implements Saveable
     private static $HOURS_ALL_TASKS = 'hours_all_tasks';
     private static $STRATEGY = 'strategy';
     private static $ALT_DEADLINE = 'alt_deadline';
-    private static $SCHEDULE_EFFORT = 'schedule_effort';
+    private static $SCHEDULE_USED = 'schedule_used';
     private static $SCHEDULE_FOLLOWED = 'schedule_followed';
-    private static $WHY_SCHEDULE_FOLLOWED = 'why_schedule_followed';
+    private static $SCHEDULE_EXPLAINED = 'schedule_explained';
     private static $WORKED_LATE_TASK = 'worked_late_task';
-    private static $WHY_WORKED_LATE_TASK = 'why_worked_late_task';
+    private static $LATE_TASK_EXPLAINED = 'late_task_explained';
     private static $ENJOYED_TASK = 'enjoyed_task';
 
     /**
@@ -67,7 +70,42 @@ class OutgoingQuestionnaireForm extends Form implements Saveable
      */
     public function save(User $user)
     {
-        // TODO: Implement save() method.
+        $outgoingSurveyForm = new OutgoingSurvey();
+        $outgoingSurveyForm->setComparedMinorAssignments($this->getIntEntry(self::$COMPARED_MINOR_ASSIGNMENTS));
+        $outgoingSurveyForm->setHoursMinorAssignment($this->getIntEntry(self::$MINOR_ASSIGNMENTS_HOURS, false));
+
+        $outgoingSurveyForm->setComparedMajorAssignments($this->getIntEntry(self::$COMPARED_MAJOR_ASSIGNMENTS));
+        $outgoingSurveyForm->setHoursMajorAssignment($this->getIntEntry(self::$MAJOR_ASSIGNMENTS_HOURS, false));
+
+        $outgoingSurveyForm->setComparedExams($this->getIntEntry(self::$COMPARED_EXAMS));
+        $outgoingSurveyForm->setHoursExams($this->getIntEntry(self::$EXAM_HOURS, false));
+
+        $outgoingSurveyForm->setComparedCoursework($this->getIntEntry(self::$COMPARED_COURSEWORK));
+        $outgoingSurveyForm->setHoursCoursework($this->getIntEntry(self::$COURSEWORK_HOURS, false));
+
+        $outgoingSurveyForm->setComparedWork($this->getIntEntry(self::$COMPARED_WORK));
+        $outgoingSurveyForm->setHoursCoursework($this->getIntEntry(self::$WORK_HOURS, false));
+
+        $outgoingSurveyForm->setComparedSocial($this->getIntEntry(self::$COMPARED_SOCIAL));
+        $outgoingSurveyForm->setHoursSocial($this->getIntEntry(self::$SOCIAL_HOURS, false));
+
+        $outgoingSurveyForm->setComparedFamily($this->getIntEntry(self::$COMPARED_FAMILY));
+        $outgoingSurveyForm->setHoursFamily($this->getIntEntry(self::$FAMILY_HOURS, false));
+
+        $outgoingSurveyForm->setHoursSpentAllTasks($this->getIntEntry(self::$FAMILY_HOURS, false));
+        $outgoingSurveyForm->setStrategy($this->getTextEntry(self::$STRATEGY, false));
+        $outgoingSurveyForm->setAltDeadline($this->getTextEntry(self::$ALT_DEADLINE, false));
+        $outgoingSurveyForm->setScheduleUsed($this->getBooleanEntry(self::$SCHEDULE_USED, false));
+        $outgoingSurveyForm->setScheduleExplained($this->getTextEntry(self::$SCHEDULE_EXPLAINED, false));
+        $outgoingSurveyForm->setLateTaskedWorkedOn($this->getBooleanEntry(self::$WORKED_LATE_TASK, false));
+        $outgoingSurveyForm->setLateTaskExplained($this->getBooleanEntry(self::$LATE_TASK_EXPLAINED, false));
+        $outgoingSurveyForm->setTaskEnjoyment($this->getIntEntry(self::$ENJOYED_TASK, false));
+
+        $outgoingSurveyForm->save();
+
+        $subject = Session::getSubject();
+        $subject->setNextState();
+        $subject->save();
     }
 
     public function getFormParameters()
@@ -124,6 +162,14 @@ class OutgoingQuestionnaireForm extends Form implements Saveable
         $validators[self::$FAMILY_HOURS] = new IntegerValidator(0, 200, false);
 
         $validators[self::$HOURS_ALL_TASKS] = new IntegerValidator(0, 200, false);
+
+        $validators[self::$SCHEDULE_USED] = new YesNoValidator(false);
+        $validators[self::$SCHEDULE_FOLLOWED] = new YesNoValidator(false);
+
+        $validators[self::$WORKED_LATE_TASK] = new YesNoValidator(false);
+
+        $validators[self::$ENJOYED_TASK]  = new SelectionValidator([1,2,3,4,5], true);
+
         return $validators;
     }
 
