d0d0e87c988aad146bc1a0d9b782aab0a700407f
Anwar A. Ruff <anwarruff@gmail.com>
2015-09-22 15:26:33 -0400

    Added additional logging

--- app/Officium/Framework/Controllers/TaskController.php
+++ app/Officium/Framework/Controllers/TaskController.php
@@ -21,28 +21,29 @@ class TaskController
         $app = Slim::getInstance();
         $subject = Session::getSubject();
 
-        EventLog::logEvent($subject, EventLog::NEW_PROBLEM_ISSUED, $taskNumber);
-
         $game = new SubjectGame($subject);
         $problem = Session::getTaskProblem($taskNumber, $subject->getId());
+        $payoff = $game->getTaskPayoff($taskNumber);
 
+        /**
+         * Other tasks are reissued when this one is worked on to prevent work on multiple tasks at once.
+         */
         $numTasks = $game->getNumTasks();
         for ($tNumber = 1; $tNumber <= $numTasks; ++$tNumber) {
             if ($tNumber == $taskNumber) {
                continue;
             }
-
             $tProblem = Session::getTaskProblem($tNumber, $subject->getId());
             $tProblem->reissue();
             Session::setTaskProblem($tProblem);
         }
 
-
-
         // Route the subject to the landing page when the current task should be re-evaluated
         if ($game->isTaskReEvaluationRequired($taskNumber) && $problem->isInInitialState()) {
             $problem->setState(Problem::PROMPT_TO_CONTINUE);
             Session::setTaskProblem($problem);
+
+            EventLog::logEvent($subject, EventLog::TASK_PENALTY_TRANSITION, $taskNumber, $payoff);
 
             $form = new Form($subject, $taskNumber, $problem);
             $form->flashProblemTransitionToPenaltyState($taskNumber);
@@ -53,10 +54,18 @@ class TaskController
         // Handle re-issuing of the problem
         if ($game->isTaskPayoffPenalized($taskNumber) && $problem->isInPromptToContinueState()) {
             $problem->setState(Problem::CONFIRMED);
+            EventLog::logEvent($subject, EventLog::PENALIZED_TASK_CHOSEN_POST_REDIRECT, $taskNumber, $payoff);
         }
         else {
             $problem->clearSolution();
             $problem->reissue();
+
+            if ($game->isTaskPayoffPenalized($taskNumber)) {
+                EventLog::logEvent($subject, EventLog::PENALIZED_PROBLEM_ISSUED, $taskNumber, $payoff);
+            }
+            else {
+                EventLog::logEvent($subject, EventLog::PROBLEM_ISSUED, $taskNumber, $payoff);
+            }
         }
 
         Session::setTaskProblem($problem);
@@ -72,17 +81,25 @@ class TaskController
     {
         $app = Slim::getInstance();
         $subject = Session::getSubject();
-
         $problem = Session::getTaskProblem($taskNumber, $subject->getId());
         $form = new Form($subject, $taskNumber, $problem);
+        $game = new SubjectGame($subject);
+        $payoff = $game->getTaskPayoff($taskNumber);
+
+        // Invalid Solution
         if ( ! $form->validate($app->request->post())) {
-            EventLog::logEvent($subject, EventLog::INCORRECT_SUBMISSION, $taskNumber);
+            if ($game->isTaskPayoffPenalized($taskNumber)) {
+                EventLog::logEvent($subject, EventLog::INCORRECT_PENALIZED_PROBLEM_SUBMITTED, $taskNumber, $payoff);
+            }
+            else {
+                EventLog::logEvent($subject, EventLog::INCORRECT_PROBLEM_SUBMITTED, $taskNumber, $payoff);
+            }
 
             $problem->setSolution($form->getSolution());
 
-            $game = new SubjectGame($subject);
             // Route the subject to the landing page when the current task should be re-evaluated
             if ($game->isTaskReEvaluationRequired($taskNumber) && $problem->isInInitialState()) {
+                EventLog::logEvent($subject, EventLog::TASK_PENALTY_TRANSITION, $taskNumber, $payoff);
                 $problem->setState(Problem::PROMPT_TO_CONTINUE);
                 Session::setTaskProblem($problem);
 
@@ -95,10 +112,19 @@ class TaskController
             return;
         }
 
-        EventLog::logEvent($subject, EventLog::CORRECT_SUBMISSION, $taskNumber);
         $form->save(Session::getUser());
+
+        if ($game->isTaskPayoffPenalized($taskNumber)) {
+            EventLog::logEvent($subject, EventLog::CORRECT_PENALIZED_PROBLEM_SUBMITTED, $taskNumber, $payoff);
+        }
+        else {
+            EventLog::logEvent($subject, EventLog::CORRECT_PROBLEM_SUBMITTED, $taskNumber, $payoff);
+        }
+
+        if ($game->areAllTasksComplete()) {
+            EventLog::logEvent($subject, EventLog::ALL_TASKS_COMPLETED);
+        }
 
         $app->redirect(LandingPageMap::toUri());
     }
-
 }
