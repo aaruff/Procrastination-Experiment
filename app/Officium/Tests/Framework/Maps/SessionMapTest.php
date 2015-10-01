e571f0ab476257915b8fcb8dbac0e4fd0a0310ba
Anwar A. Ruff <anwarruff@gmail.com>
2015-10-01 16:37:10 -0400

    Removed unused classes and their tests

--- app/Officium/Tests/Framework/Maps/SessionMapTest.php
+++ app/Officium/Tests/Framework/Maps/SessionMapTest.php
@@ -12,14 +12,6 @@ class SessionMapTest extends \PHPUnit_Framework_TestCase {
     /**
      * @test
      */
-    public function When_UriCalled_UriReturned()
-    {
-        $this->assertEquals('/experiment/session/add', SessionMap::toUri());
-    }
-
-    /**
-     * @test
-     */
     public function When_PathToControllerCalled_PathReturned()
     {
         $controller = get_class(new SessionController());
