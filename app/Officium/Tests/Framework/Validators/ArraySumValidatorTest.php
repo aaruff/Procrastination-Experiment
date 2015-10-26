acd6a7cc8fe6bd8d1afa4dc38d6f4809609ef233
aaruff <anwarruff@gmail.com>
2015-10-26 18:09:22 -0400

    Added tests for the ArraySumValidator

--- app/Officium/Tests/Framework/Validators/ArraySumValidatorTest.php
+++ app/Officium/Tests/Framework/Validators/ArraySumValidatorTest.php
@@ -4,7 +4,26 @@
 namespace Framework\Validators;
 
 
+use Officium\Framework\Validators\ArraySumValidator;
+
 class ArraySumValidatorTest extends \PHPUnit_Framework_TestCase
 {
+    /**
+     * @test
+     */
+    public function Given_ValidSum_When_Validated_Should_ReturnTrue()
+    {
+        $validator = new ArraySumValidator('values', 100);
+        $this->assertTrue($validator->validate(['values'=>[1,99]]));
+    }
+
+    /**
+     * @test
+     */
+    public function Given_InvalidSum_When_Validated_Should_ReturnFalse()
+    {
+        $validator = new ArraySumValidator('values', 100);
+        $this->assertFalse($validator->validate(['values'=>[1,91]]));
+    }
 
 }
