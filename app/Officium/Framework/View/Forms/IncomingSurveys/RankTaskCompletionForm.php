54985d4d3967f0d5aa0b808ef5545a367dccd79e
aaruff <anwarruff@gmail.com>
2015-10-26 18:09:08 -0400

    Modified parameters to support an array entry

--- app/Officium/Framework/View/Forms/IncomingSurveys/RankTaskCompletionForm.php
+++ app/Officium/Framework/View/Forms/IncomingSurveys/RankTaskCompletionForm.php
@@ -3,21 +3,30 @@
 namespace Officium\Framework\View\Forms\IncomingSurveys;
 
 use Officium\Experiment\RankTaskCompletionSurvey;
+use Officium\Framework\Validators\ArraySumValidator;
+use Officium\Framework\Validators\ArrayValidator;
 use Officium\Framework\Validators\IntegerValidator;
 use Officium\Framework\Models\Saveable;
 use Officium\Framework\Models\User;
 use Officium\Framework\View\Forms\Form;
+use Slim\Slim;
 
 class RankTaskCompletionForm extends Form implements Saveable
 {
-    private static $NO_TASK = 'n';
-    private static $ONE_TASK = 'o';
-    private static $TWO_TASKS = 't';
-    private static $ALL_TASKS = 'a';
+    private static $SCALE = 'scale';
 
     /* ------------------------------------------------------------------------------------------
      *                                      Public
      * ------------------------------------------------------------------------------------------ */
+    public function getFormParameters()
+    {
+        $app = Slim::getInstance();
+        $parameters = [
+            'num_tasks' => ['no tasks', 'one task', 'two tasks', 'three tasks']
+        ];
+
+        return $parameters;
+    }
 
     /**
      * Stores properties to the session.
@@ -28,13 +37,15 @@ class RankTaskCompletionForm extends Form implements Saveable
      */
     public function save(User $user)
     {
-        $survey = new RankTaskCompletionSurvey();
-        $survey->setNoTaskRank($this->getIntEntry(self::$NO_TASK));
-        $survey->setOneTaskRank($this->getIntEntry(self::$ONE_TASK));
-        $survey->setTwoTaskRank($this->getIntEntry(self::$TWO_TASKS));
-        $survey->setAllTaskRank($this->getIntEntry(self::$ALL_TASKS));
-        $survey->setSubjectId($user->getSubject()->getId());
-        $survey->save();
+        $scale = $this->getIntArrayEntry(self::$SCALE);
+        $subjectId = $user->getSubject()->getId();
+        foreach ($scale as $numCompleted => $rank) {
+            $survey = new RankTaskCompletionSurvey();
+            $survey->setNumberCompleted($numCompleted);
+            $survey->setRank($rank);
+            $survey->setSubjectId($subjectId);
+            $survey->save();
+        }
     }
 
     /* ------------------------------------------------------------------------------------------
@@ -49,10 +60,8 @@ class RankTaskCompletionForm extends Form implements Saveable
     protected function getValidators()
     {
         $validators = [];
-        $validators[self::$NO_TASK] = new IntegerValidator(0, 100);
-        $validators[self::$ONE_TASK] = new IntegerValidator(0, 100);
-        $validators[self::$TWO_TASKS] = new IntegerValidator(0, 100);
-        $validators[self::$ALL_TASKS] = new IntegerValidator(0, 100);
+        $validators[self::$SCALE] = new ArrayValidator(self::$SCALE, new IntegerValidator(0, 100));
+        $validators[self::$SEMANTIC_VALIDATORS] = [self::$SCALE => new ArraySumValidator(self::$SCALE, 100)];
         return $validators;
     }
 }
