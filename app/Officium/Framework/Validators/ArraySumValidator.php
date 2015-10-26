4348d77b00142ad1fd262cd67180f713e97c5c0b
aaruff <anwarruff@gmail.com>
2015-10-26 18:04:57 -0400

    Created validator

--- app/Officium/Framework/Validators/ArraySumValidator.php
+++ app/Officium/Framework/Validators/ArraySumValidator.php
@@ -3,7 +3,48 @@
 namespace Officium\Framework\Validators;
 
 
-class ArraySumValidator
+class ArraySumValidator extends Validator implements SymanticValidator
 {
+    private $key;
+    private $sum;
 
+    public function __construct($key, $sum)
+    {
+        $this->key = $key;
+        $this->sum = $sum;
+    }
+
+    /**
+     * Validates the entry provided. Note: Validator assumes all key entries are numeric values.
+     * @param $entries
+     * @return boolean
+     */
+    public function validate($entries)
+    {
+        $this->clearErrors();
+
+        if ( ! isset($entries[$this->key]) || empty($entries[$this->key]) || ! is_array($entries[$this->key])) {
+            $this->setErrors([$this->key => 'Entry required.']);
+            return false;
+        }
+
+        $values = $entries[$this->key];
+
+        $runningSum = 0;
+        foreach ($values as $value) {
+            $runningSum += $value;
+        }
+
+        if ($runningSum != $this->sum) {
+            $this->setErrors([$this->key => 'All entries must sum to ' . $this->sum]);
+            return false;
+        }
+
+        return true;
+    }
+
+    public function getEntryType()
+    {
+        return Validator::$ALL_ENTRIES;
+    }
 }
