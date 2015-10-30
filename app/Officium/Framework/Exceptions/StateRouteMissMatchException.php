2b489964265b212fd92f0d70cef5bb6856a5aab7
aaruff <anwarruff@gmail.com>
2015-10-30 15:19:21 -0400

    Added route mismatch exception

--- app/Officium/Framework/Exceptions/StateRouteMissMatchException.php
+++ app/Officium/Framework/Exceptions/StateRouteMissMatchException.php
@@ -3,7 +3,13 @@
 namespace Officium\Framework\Exceptions;
 
 
-class StateRouteMissMatchException
+class StateRouteMissMatchException extends \RuntimeException
 {
+    function __construct($uri, $expectedUri)
+    {
+        $message = "Invalid state route: (" . $uri . "). The current state route is: (" . $expectedUri .").";
+        $code = 404;
+        parent::__construct($message, $code);
+    }
 
 }
