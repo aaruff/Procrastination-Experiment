1803a97f725b247422036a63124078ce26ffa25a
aaruff <anwarruff@gmail.com>
2015-10-30 15:18:42 -0400

    Added invalid route exception

--- app/Officium/Framework/Exceptions/InvalidRouteException.php
+++ app/Officium/Framework/Exceptions/InvalidRouteException.php
@@ -3,7 +3,13 @@
 namespace Officium\Framework\Exceptions;
 
 
-class InvalidRouteException
+class InvalidRouteException extends \RuntimeException
 {
+    function __construct($uri)
+    {
+        $message = "Invalid route requested (" . $uri . ")";
+        $code = 404;
+        parent::__construct($message, $code);
+    }
 
 }
